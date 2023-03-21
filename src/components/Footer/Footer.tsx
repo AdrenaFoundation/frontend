c253e0d6869110fc93b0b1e86f3c618c5b33ac9c
Orex <adrena.orex@proton.me>
2023-03-21 10:46:31 +0400

    nit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,22 @@
 import styles from "./Footer.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-  return <div className={styles.footer}></div>;
+  return (
+    <div className={styles.footer}>
+      <Link
+        href="https://github.com/orgs/AdrenaDEX/repositories"
+        target="_blank"
+      >
+        <Image
+          className={styles.footer__image}
+          src="/images/github.svg"
+          alt="github icon"
+          width="25"
+          height="25"
+        />
+      </Link>
+    </div>
+  );
 }
