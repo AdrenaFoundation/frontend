22c861d2661ba30856f1a7182a54500725c8a39d
tyro-adrena <139131552+tyro-adrena@users.noreply.github.com>
2023-08-14 21:38:36 +0200

    move custom fonts

--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -22,12 +22,6 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <meta name="description" content="Insert Description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&family=JetBrains+Mono:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       {isBigScreen ? <Header /> : <BurgerMenu />}
