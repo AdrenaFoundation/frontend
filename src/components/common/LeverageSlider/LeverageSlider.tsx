80bdc53b29cf617253ef933c19f2f7e51525e550
kino <adrena.kino@gmail.com>
2024-04-18 14:45:51 +0200

    Restyle elements and fade leverage number to indicate it's an input

--- src/components/common/LeverageSlider/LeverageSlider.tsx
+++ src/components/common/LeverageSlider/LeverageSlider.tsx
@@ -26,86 +26,85 @@ export default function LeverageSlider({
   const marks = ['x3', 'x5', 'x10', 'x25', 'x50', 'x100'];
 
   return (
-    <div className={twMerge('flex overflow-hidden h-16', className)}>
-      <div className="flex pl-1 pt-1 pr-2 pb-1 bg-third w-[4em] border-r h-full">
-        <div className="flex w-full items-center ml-1 shrink-0">
-          <span className="shrink-0 w-2">x</span>
+    <div className={twMerge('flex flex-col overflow-hidden', className)}>
+      <div className="flex">
+        <div className="flex flex-col pl-1 pt-1 pr-2 pb-1 bg-third w-[4.5em] h-14">
+          <div className="flex w-full h-full items-center ml-1 shrink-0">
+            <span className="shrink-0 w-2">x</span>
 
-          <InputNumber
-            className="flex flex-col w-full max-w-full overflow-hidden text-center bg-third"
-            value={isLeverageInputEmpty ? undefined : value}
+            <InputNumber
+              className="flex w-full max-w-full overflow-hidden text-center bg-third text-txtfade"
+              value={isLeverageInputEmpty ? undefined : value}
+              max={100}
+              onChange={function (value: number | null): void {
+                // throw new Error('Function not implemented.');
+                if (value === null) {
+                  setIsLeverageInputEmpty(true);
+                  return;
+                }
+
+                onChange(value);
+                setIsLeverageInputEmpty(false);
+              }}
+              inputFontSize="1em"
+            />
+          </div>
+        </div>
+
+        <div className="flex h-full w-full bg-transparent pr-4 pl-4 border-r">
+          <Slider
+            className="relative top-6"
+            min={1}
             max={100}
-            onChange={function (value: number | null): void {
-              // throw new Error('Function not implemented.');
-              if (value === null) {
-                setIsLeverageInputEmpty(true);
-                return;
-              }
-
-              onChange(value);
-              setIsLeverageInputEmpty(false);
+            value={value}
+            defaultValue={1}
+            step={0.1}
+            railStyle={{
+              backgroundColor: colorA,
+              height: 2,
             }}
-            inputFontSize="1em"
+            trackStyle={{
+              backgroundColor: 'white',
+              height: 2,
+            }}
+            handleStyle={{
+              backgroundColor: 'white',
+              borderColor: 'white',
+              opacity: 1,
+              borderWidth: '3px',
+            }}
+            activeDotStyle={{
+              backgroundColor: 'white',
+              borderColor: colorA,
+            }}
+            dotStyle={{
+              borderRadius: 0,
+              width: '2px',
+              border: 0,
+              height: '0.8em',
+              backgroundColor: colorA,
+              borderColor: colorA,
+            }}
+            // Use as number because we don't use the slider as a range
+            onChange={(v) => onChange(v as number)}
           />
         </div>
       </div>
-
-      <div className="flex flex-col h-full w-full bg-transparent pr-4 pl-2 ml-2">
-        <Slider
-          className="relative top-4"
-          min={1}
-          max={100}
-          value={value}
-          defaultValue={1}
-          step={0.1}
-          railStyle={{
-            backgroundColor: colorA,
-            height: 2,
-          }}
-          trackStyle={{
-            backgroundColor: 'white',
-            height: 2,
-          }}
-          handleStyle={{
-            backgroundColor: 'white',
-            borderColor: 'white',
-            opacity: 1,
-            borderWidth: '3px',
-          }}
-          activeDotStyle={{
-            backgroundColor: 'white',
-            borderColor: colorA,
-          }}
-          dotStyle={{
-            borderRadius: 0,
-            width: '2px',
-            border: 0,
-            height: '0.8em',
-            backgroundColor: colorA,
-            borderColor: colorA,
-          }}
-          // Use as number because we don't use the slider as a range
-          onChange={(v) => onChange(v as number)}
-        />
-        {/*         <div className="grid grid-cols-11 gap-4 mt-4"> */}
-        <div className="flex bg-secondary rounded-full mt-5">
-          {marks.map((mark, index) => (
-            <Button
-              key={index}
-              title={mark}
-              variant="secondary"
-              className={twMerge(
-                'w-[2.5em] h-[2em] opacity-50 hover:opacity-100 flex-grow rounded-none text-xs border border-bcolor',
-                index === 0 ? 'rounded-l-full' : '',
-                index === marks.length - 1 ? 'rounded-r-full' : '',
-              )}
-              onClick={() => {
-                onChange(Number(mark.slice(1)));
-                setIsLeverageInputEmpty(false);
-              }}
-            ></Button>
-          ))}
-        </div>
+      <div className="flex h-7">
+        {marks.map((mark, index) => (
+          <Button
+            key={index}
+            title={mark}
+            variant="secondary"
+            className={twMerge(
+              'w-[6em] opacity-50 hover:opacity-100 flex-grow rounded-none text-xs border border-bcolor',
+            )}
+            onClick={() => {
+              onChange(Number(mark.slice(1)));
+              setIsLeverageInputEmpty(false);
+            }}
+          ></Button>
+        ))}
       </div>
     </div>
   );
