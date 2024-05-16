4223f5fd700aee19bbc2723eafd75de07c0ec57d
kino <adrena.kino@gmail.com>
2024-05-16 10:03:22 +0200

    Lessen create profile tab

--- src/components/pages/user_profile/ProfileCreation.tsx
+++ src/components/pages/user_profile/ProfileCreation.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/common/Button/Button';
 import InputString from '@/components/common/inputString/InputString';
+import InfoAnnotation from '../monitoring/InfoAnnotation';
 
 export default function ProfileCreation({
   initUserProfile,
@@ -12,40 +13,42 @@ export default function ProfileCreation({
 }) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center z-20 border bg-bcolor/85 backdrop-blur-md p-7 m-4 w-[25em] self-center rounded-lg">
-        <div className="font-special text-3xl text-center">
-          Create my profile
-        </div>
-
-        <span className="mt-6 max-w-[28em] flex text-center opacity-75 italic text-lg">
-          Profile&apos;s optional – no need for trading, swapping, or staking.
-          Handy for tracking your stats: average leverage, PnL, fees, and more.
-        </span>
-
-        <div className="w-2/3 h-[1px] bg-bcolor mt-8"></div>
-
-        <div className="flex flex-col items-center justify-center">
-          <div className="font-special text-xl mt-10 ">My Nickname</div>
-
-          <InputString
-            value={nickname ?? ''}
-            onChange={setNickname}
-            placeholder="The Great Trader"
-            className="mt-4 text-center w-[20em] p-4 bg-third border rounded-xl"
-            inputFontSize="1.1em"
-            maxLength={24}
+      <div className="flex flex-col w-full z-20 border bg-bcolor/85 backdrop-blur-md p-7 self-center rounded-lg">
+        <div className="flex">
+          <h2>Create my profile (optional)</h2>
+          <InfoAnnotation
+            text="Profile's optional – no need for trading, swapping, or staking.
+          Handy for tracking your stats: average leverage, PnL, fees, and more."
+            className="mr-1"
           />
         </div>
 
-        <Button
-          disabled={
-            nickname ? !(nickname.length >= 3 && nickname.length <= 24) : true
-          }
-          className="mt-4 text-sm w-full"
-          size="lg"
-          title="Create"
-          onClick={() => initUserProfile()}
-        />
+        <div className="flex flex-col mt-4">
+          <h5 className="flex flex-col text-fade mb-3">Nickname</h5>
+          <div className="flex">
+            <InputString
+              value={nickname ?? ''}
+              onChange={setNickname}
+              placeholder="The Great Trader"
+              className="text-center w-[80%] bg-inputcolor border rounded-xl"
+              inputFontSize="1.5em"
+              maxLength={24}
+            />
+
+            <Button
+              disabled={
+                nickname
+                  ? !(nickname.length >= 3 && nickname.length <= 24)
+                  : true
+              }
+              className="text-sm w-[20%] ml-4"
+              size="lg"
+              variant="primary"
+              title="Create"
+              onClick={() => initUserProfile()}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
