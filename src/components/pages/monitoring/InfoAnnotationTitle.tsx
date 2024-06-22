a06f4d2b11e12e0a50b6184b5c54e4c4f216f9ef
Orex <adrena.orex@gmail.com>
2024-06-22 10:27:37 +0200

    Fix

--- src/components/pages/monitoring/InfoAnnotationTitle.tsx
+++ src/components/pages/monitoring/InfoAnnotationTitle.tsx
@@ -13,22 +13,24 @@ export default function InfoAnnotationTitle({
   text: ReactNode;
   className: string;
   title: string;
-}) (
-  <Tippy
-    content={
-      <div className="text-sm w-60 flex flex-col justify-around">{text}</div>
-    }
-    placement="auto"
-  >
-    <div className="h-auto w-auto flex items-center justify-center">
-      <h3 className="pr-1">{title}</h3>
-      <Image
-        className={twMerge('opacity-50 hover:opacity-100', className)}
-        src={infoIcon}
-        alt="info icon"
-        width="16"
-        height="16"
-      />
-    </div>
-  </Tippy>
-);
+}) {
+  return (
+    <Tippy
+      content={
+        <div className="text-sm w-60 flex flex-col justify-around">{text}</div>
+      }
+      placement="auto"
+    >
+      <div className="h-auto w-auto flex items-center justify-center">
+        <h3 className="pr-1">{title}</h3>
+        <Image
+          className={twMerge('opacity-50 hover:opacity-100', className)}
+          src={infoIcon}
+          alt="info icon"
+          width="16"
+          height="16"
+        />
+      </div>
+    </Tippy>
+  );
+}
