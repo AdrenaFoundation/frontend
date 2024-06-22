3982be26bda4ed09fb1ef66015ed20d4b8215dd0
Gregory Neut <gregoryneut@Gregorys-MBP.localdomain>
2024-06-22 10:23:45 +0200

    Fix

--- src/components/pages/monitoring/InfoAnnotation.tsx
+++ src/components/pages/monitoring/InfoAnnotation.tsx
@@ -13,22 +13,24 @@ export default function InfoAnnotation({
   text: ReactNode;
   className: string;
   title?: string;
-}) (
-  <Tippy
-    content={
-      <div className="text-sm w-60 flex flex-col justify-around">{text}</div>
-    }
-    placement="auto"
-  >
-    <div className="h-auto w-auto flex items-center justify-center">
-      {title ? <h3>{title}</h3> : <></>}
-      <Image
-        className={twMerge('opacity-50 hover:opacity-100 ml-1', className)}
-        src={infoIcon}
-        alt="info icon"
-        width="10"
-        height="10"
-      />
-    </div>
-  </Tippy>
-);
+}) {
+  return (
+    <Tippy
+      content={
+        <div className="text-sm w-60 flex flex-col justify-around">{text}</div>
+      }
+      placement="auto"
+    >
+      <div className="h-auto w-auto flex items-center justify-center">
+        {title ? <h3>{title}</h3> : <></>}
+        <Image
+          className={twMerge('opacity-50 hover:opacity-100 ml-1', className)}
+          src={infoIcon}
+          alt="info icon"
+          width="10"
+          height="10"
+        />
+      </div>
+    </Tippy>
+  );
+}
