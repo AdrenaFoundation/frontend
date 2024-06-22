9bca30dfa0f67f1d23d8ea2ebb76aab834836fc0
Orex <adrena.orex@gmail.com>
2024-06-22 10:29:30 +0200

    Fix

--- src/components/pages/monitoring/TitleAnnotation.tsx
+++ src/components/pages/monitoring/TitleAnnotation.tsx
@@ -1,3 +1,3 @@
-export default function TitleAnnotation({ text }: { text: string }) (
-  <span className="text-[0.8em] text-txtfade ml-1">{text}</span>
-);
+export default function TitleAnnotation({ text }: { text: string }) {
+  return <span className="text-[0.8em] text-txtfade ml-1">{text}</span>;
+}
