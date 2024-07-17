e281201180c119936b41da2e748a495e451b3fc8
adrena-kino <adrena.kino@gmail.com>
2024-07-17 17:23:43 +0700

    Fix typo on open position if short or long

--- src/components/pages/trading/TradingInputs/LongShortTradingInputs.tsx
+++ src/components/pages/trading/TradingInputs/LongShortTradingInputs.tsx
@@ -128,7 +128,7 @@ export default function LongShortTradingInputs({
     }
 
     const notification = MultiStepNotification.newForRegularTransaction(
-      'Long Position Opening',
+      side.charAt(0).toUpperCase() + side.slice(1) + 'Position Opening',
     ).fire();
 
     // Existing position or not, it's the same
