bf4a94ae0d69e1eb325ddf20aacd049f101510e2
Tyro <139131552+adrena-tyro@users.noreply.github.com>
2024-09-17 14:03:31 +0200

    nit (#147)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,16 @@ export const metadata = {
   description: 'Trade at the speed of light with up to 100x leverage',
   openGraph: {
     title: 'Adrena',
-    description: 'Trade at the speed of light with up to 100x leverage',
+    description:
+      'Get bonus $ADX for being first to seed liquidity to the Adrena Liquidity Pool',
     images:
       'https://iyd8atls7janm7g4.public.blob.vercel-storage.com/adrena_genesis_og-tXy102rrl9HR0SfCsj0d4LywnaXTJM.jpg',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Adrena',
-    description: 'Trade at the speed of light with up to 100x leverage',
+    description:
+      'Get bonus $ADX for being first to seed liquidity to the Adrena Liquidity Pool',
     creator: '@adrenaprotocol',
     images:
       'https://iyd8atls7janm7g4.public.blob.vercel-storage.com/adrena_genesis_og-tXy102rrl9HR0SfCsj0d4LywnaXTJM.jpg',
