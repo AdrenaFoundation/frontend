069439e63ebdc6bade0ab0cdef3bdcb9804b6767
Orex <adrena.orex@gmail.com>
2024-09-25 01:45:02 +0400

    Add pool Pnl

--- src/pages/monitoring/basic.tsx
+++ src/pages/monitoring/basic.tsx
@@ -4,14 +4,12 @@ import StyledContainer from '@/components/common/StyledContainer/StyledContainer
 import AumChart from '@/components/pages/global/Aum/AumChart';
 import CompositionChart from '@/components/pages/global/Composition/CompositionChart';
 import OpenInterestChart from '@/components/pages/global/OpenInterest/OpenInterestChart';
+import UnrealizedPnlChart from '@/components/pages/global/UnrealizedPnl/UnrealizedPnlChart';
 import UtilizationChart from '@/components/pages/global/UtilizationChart/UtilizationChart';
 import { PoolInfo } from '@/hooks/usePoolInfo';
 import { PageProps } from '@/types';
 
-export default function BasicMonitoring({
-  mainPool,
-  poolInfo,
-}: PageProps & {
+export default function BasicMonitoring({}: PageProps & {
   poolInfo: PoolInfo | null;
 }) {
   return (
@@ -25,6 +23,10 @@ export default function BasicMonitoring({
         <div className="grid lg:grid-cols-2 gap-16 lg:h-[18em]">
           <CompositionChart />
           <OpenInterestChart />
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-16 lg:h-[18em]">
+          <UnrealizedPnlChart />
         </div>
       </StyledContainer>
     </div>
