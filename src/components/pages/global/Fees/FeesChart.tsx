ccd4a11c1d8c531279de61778d4aa953ec8c6269
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-09-25 00:21:38 +0200

    fees chart

---
+++ src/components/pages/global/Fees/FeesChart.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+
+import LineRechartFees from './LineRechartFees';
+
+export default function FeesChart() {
+  const [chartData, setChartData] = useState<any>(null);
+
+  useEffect(() => {
+    getPoolInfo();
+  }, []);
+
+  const getPoolInfo = async () => {
+    try {
+      const res = await fetch(
+        'https://datapi.adrena.xyz/poolinfo?cumulative_swap_fee_usd=true&cumulative_liquidity_fee_usd=true&cumulative_close_position_fee_usd=true&cumulative_liquidation_fee_usd=true&cumulative_borrow_fee_usd=true',
+      );
+      const { data } = await res.json();
+      const {
+        cumulative_swap_fee_usd,
+        cumulative_liquidity_fee_usd,
+        cumulative_close_position_fee_usd,
+        cumulative_liquidation_fee_usd,
+        cumulative_borrow_fee_usd,
+        snapshot_timestamp,
+      } = data;
+
+      const timeStamp = snapshot_timestamp.map((time: string) =>
+        new Date(time).toLocaleTimeString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+        }),
+      );
+
+      const formattedData = timeStamp.map(
+        (time: number, i: string | number) => ({
+          name: time,
+          ['Swap And Fee']: cumulative_swap_fee_usd[i],
+          ['Liquidity Fee']: cumulative_liquidity_fee_usd[i],
+          ['Close Position Fee']: cumulative_close_position_fee_usd[i],
+          ['Liquidation Fee']: cumulative_liquidation_fee_usd[i],
+          ['Borrow Fee']: cumulative_borrow_fee_usd[i],
+        }),
+      );
+
+      setChartData(formattedData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  if (!chartData) {
+    return (
+      <div className="h-full w-full flex items-center justify-center text-sm">
+        Loading...
+      </div>
+    );
+  }
+
+  return (
+    <LineRechartFees
+      title={'Cumulative Fees'}
+      data={chartData}
+      labels={[
+        {
+          name: 'Swap And Fee',
+          color: '#f7931a',
+        },
+        {
+          name: 'Liquidity Fee',
+          color: '#2775ca',
+        },
+        {
+          name: 'Close Position Fee',
+          color: '#84CC90',
+        },
+        {
+          name: 'Liquidation Fee',
+          color: '#BE84CC',
+        },
+        {
+          name: 'Borrow Fee',
+          color: '#DA6F71',
+        },
+      ]}
+    />
+  );
+}
--- src/pages/monitoring/basic.tsx
+++ src/pages/monitoring/basic.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import StyledContainer from '@/components/common/StyledContainer/StyledContainer';
 import AumChart from '@/components/pages/global/Aum/AumChart';
 import CompositionChart from '@/components/pages/global/Composition/CompositionChart';
+import CumulativePnlChart from '@/components/pages/global/CumulativePnl/CumulativePnlChart';
+import FeesChart from '@/components/pages/global/Fees/FeesChart';
 import OpenInterestChart from '@/components/pages/global/OpenInterest/OpenInterestChart';
 import UnrealizedPnlChart from '@/components/pages/global/UnrealizedPnl/UnrealizedPnlChart';
 import UtilizationChart from '@/components/pages/global/UtilizationChart/UtilizationChart';
 import { PoolInfo } from '@/hooks/usePoolInfo';
 import { PageProps } from '@/types';
-import CumulativePnlChart from '@/components/pages/global/CumulativePnl/CumulativePnlChart';
 
 export default function BasicMonitoring({}: PageProps & {
   poolInfo: PoolInfo | null;
@@ -29,6 +30,10 @@ export default function BasicMonitoring({}: PageProps & {
         <div className="grid lg:grid-cols-2 gap-16 lg:h-[18em]">
           <UnrealizedPnlChart />
           <CumulativePnlChart />
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-16 lg:h-[18em]">
+          <FeesChart />
         </div>
       </StyledContainer>
     </div>
