8e8d130ce12381aa353684f76356db6bde85d1e6
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-10-03 11:12:22 +0200

    test og image

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from '@vercel/og';
+import Head from 'next/head';
 
 export const config = {
   runtime: 'edge',
@@ -7,21 +8,36 @@ export const config = {
 export default async function handler() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        test
-      </div>
+      <>
+        <Head>
+          <title>Test</title>
+          <meta
+            property="og:image"
+            content="https://frontend-git-pnlshare-adrena.vercel.app/api/og"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta
+            name="twitter:image"
+            content="https://frontend-git-pnlshare-adrena.vercel.app/api/og"
+          />
+        </Head>
+        <div
+          style={{
+            fontSize: 40,
+            color: 'black',
+            background: 'white',
+            width: '100%',
+            height: '100%',
+            padding: '50px 200px',
+            textAlign: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          test
+        </div>
+      </>
     ),
     {
       width: 1200,
