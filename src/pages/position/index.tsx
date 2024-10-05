7fc29879b76ea9c8c0fb44ecf7f8414d78e38add
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-10-05 20:11:44 +0200

    metatag test pt2

--- src/pages/position/index.tsx
+++ src/pages/position/index.tsx
@@ -1,12 +1,19 @@
 import Head from 'next/head';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function position() {
-  //   useEffect(() => {}, []);
+  const searchParams = new URLSearchParams(window.location.search);
+
+  const position = searchParams.get('po') ?? 'Adrena';
+  const img =
+    searchParams.get('po') ??
+    'https://iyd8atls7janm7g4.public.blob.vercel-storage.com/adrena-position-g.png';
+  console.log(position);
+
   return (
     <div>
       <Head>
-        <title>setIsDownloading</title>
+        <title>{position}</title>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@adrenaprotocol" />
         <meta name="twitter:creator" content="@adrenaprotocol" />
@@ -15,10 +22,7 @@ export default function position() {
           name="twitter:description"
           content="Get bonus $ADX for being first to seed liquidity to the Adrena Liquidity Pool"
         />
-        <meta
-          name="twitter:image"
-          content="https://iyd8atls7janm7g4.public.blob.vercel-storage.com/adrena-position-g.png"
-        />
+        <meta name="twitter:image" content={img} />
       </Head>
     </div>
   );
