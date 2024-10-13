e34f6b24757700fae4818294e3283d00becd141c
Orex <adrena.orex@gmail.com>
2024-10-13 17:22:54 +0400

    Increase priority fee

--- src/AdrenaClient.ts
+++ src/AdrenaClient.ts
@@ -273,7 +273,7 @@ export class AdrenaClient {
     public custodies: CustodyExtended[],
     public tokens: Token[],
     public genesisLockPda: PublicKey,
-  ) { }
+  ) {}
 
   public setReadonlyAdrenaProgram(program: Program<Adrena>) {
     this.readonlyAdrenaProgram = program;
@@ -427,14 +427,14 @@ export class AdrenaClient {
       .map((custody, i) => {
         const infos:
           | {
-            name: string;
-            color: string;
-            symbol: string;
-            image: ImageRef;
-            coingeckoId: string;
-            decimals: number;
-            pythPriceUpdateV2: PublicKey;
-          }
+              name: string;
+              color: string;
+              symbol: string;
+              image: ImageRef;
+              coingeckoId: string;
+              decimals: number;
+              pythPriceUpdateV2: PublicKey;
+            }
           | undefined = config.tokensInfo[custody.mint.toBase58()];
 
         if (!infos) {
@@ -1628,13 +1628,13 @@ export class AdrenaClient {
     const { swappedTokenDecimals, swappedTokenPrice } =
       side === 'long'
         ? {
-          swappedTokenDecimals: tokenB.decimals,
-          swappedTokenPrice: tokenBPrice,
-        }
+            swappedTokenDecimals: tokenB.decimals,
+            swappedTokenPrice: tokenBPrice,
+          }
         : {
-          swappedTokenDecimals: usdcToken.decimals,
-          swappedTokenPrice: usdcTokenPrice,
-        };
+            swappedTokenDecimals: usdcToken.decimals,
+            swappedTokenPrice: usdcTokenPrice,
+          };
 
     const swapFeeUsd =
       nativeToUi(swapFeeIn, tokenA.decimals) * tokenAPrice +
@@ -1760,9 +1760,9 @@ export class AdrenaClient {
     const transaction = await (position.side === 'long'
       ? this.buildAddCollateralLongTx.bind(this)
       : this.buildAddCollateralShortTx.bind(this))({
-        position,
-        collateralAmount: addedCollateral,
-      })
+      position,
+      collateralAmount: addedCollateral,
+    })
       .preInstructions(preInstructions)
       .postInstructions(postInstructions)
       .transaction();
@@ -2495,11 +2495,11 @@ export class AdrenaClient {
         stakeResolutionThreadId: lockedStake.stakeResolutionThreadId,
         amount: additionalAmount
           ? uiToNative(
-            additionalAmount,
-            lockedStake.tokenSymbol === 'ALP'
-              ? this.alpToken.decimals
-              : this.adxToken.decimals,
-          )
+              additionalAmount,
+              lockedStake.tokenSymbol === 'ALP'
+                ? this.alpToken.decimals
+                : this.adxToken.decimals,
+            )
           : null,
         lockedDays: updatedDuration ?? null,
       })
@@ -3945,13 +3945,13 @@ export class AdrenaClient {
         )
           ? new BN(0)
           : uiToNative(
-            collateralTokenPriceUi *
-            nativeToUi(
-              position.nativeObject.lockedAmount,
-              collateralCustody.tokenInfo.decimals,
-            ),
-            USD_DECIMALS,
-          );
+              collateralTokenPriceUi *
+                nativeToUi(
+                  position.nativeObject.lockedAmount,
+                  collateralCustody.tokenInfo.decimals,
+                ),
+              USD_DECIMALS,
+            );
 
         return {
           profitUsd: nativeToUi(
@@ -4105,7 +4105,9 @@ export class AdrenaClient {
             collateralCustody: position.collateralCustody,
             owner: position.owner,
             pubkey: possiblePositionAddresses[index],
-            initialLeverage: nativeToUi(position.sizeUsd, USD_DECIMALS) / nativeToUi(position.collateralUsd, USD_DECIMALS),
+            initialLeverage:
+              nativeToUi(position.sizeUsd, USD_DECIMALS) /
+              nativeToUi(position.collateralUsd, USD_DECIMALS),
             currentLeverage: null,
             token,
             collateralToken,
@@ -4125,9 +4127,9 @@ export class AdrenaClient {
             stopLossClosePositionPrice:
               position.stopLossThreadIsSet === 1
                 ? nativeToUi(
-                  position.stopLossClosePositionPrice,
-                  PRICE_DECIMALS,
-                )
+                    position.stopLossClosePositionPrice,
+                    PRICE_DECIMALS,
+                  )
                 : null,
             stopLossLimitPrice:
               position.stopLossThreadIsSet === 1
@@ -4545,7 +4547,7 @@ export class AdrenaClient {
 
       transaction.instructions.unshift(
         ComputeBudgetProgram.setComputeUnitPrice({
-          microLamports: 10000, // 700k compute unit for 7000 lamports
+          microLamports: 30000, // 700k compute unit for 21000 lamports (~4 tx price)
         }),
         ComputeBudgetProgram.setComputeUnitLimit({
           units: 1000000, // Use a lot of units to avoid any issues during simulation
@@ -4616,7 +4618,8 @@ export class AdrenaClient {
     notification?.currentStepSucceeded();
 
     console.log(
-      `tx: https://explorer.solana.com/tx/${txHash}${this.config.cluster === 'devnet' ? '?cluster=devnet' : ''
+      `tx: https://explorer.solana.com/tx/${txHash}${
+        this.config.cluster === 'devnet' ? '?cluster=devnet' : ''
       }`,
     );
 
