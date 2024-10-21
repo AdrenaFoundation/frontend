81fc7d639afc284c597bfa38c9ab2dc94c5d0da5
adrena-kino <adrena.kino@gmail.com>
2024-10-21 11:45:00 +0200

    handle value label for tooltip chart

--- src/components/CustomRechartsToolTip/CustomRechartsToolTip.tsx
+++ src/components/CustomRechartsToolTip/CustomRechartsToolTip.tsx
@@ -6,14 +6,18 @@ import {
 } from 'recharts/types/component/DefaultTooltipContent';
 import { twMerge } from 'tailwind-merge';
 
-import { formatPriceInfo } from '@/utils';
+import { formatPercentage, formatPriceInfo } from '@/utils';
 
 export default function CustomRechartsToolTip({
   active,
   payload,
   label,
   isValueOnly,
-}: TooltipProps<ValueType, NameType> & { isValueOnly?: boolean }) {
+  format = 'currency',
+}: TooltipProps<ValueType, NameType> & {
+  isValueOnly?: boolean;
+  format?: 'currency' | 'percentage' | 'number';
+}) {
   if (active && payload && payload.length) {
     return (
       <div className="bg-third p-3 border border-white rounded-lg min-w-[12em]">
@@ -31,7 +35,11 @@ export default function CustomRechartsToolTip({
               className={twMerge('font-mono', isValueOnly && 'text-lg')}
               style={{ color: item.color }}
             >
-              {formatPriceInfo(Number(item.value), 2, 2)}
+              {format === 'currency'
+                ? formatPriceInfo(Number(item.value), 2, 2)
+                : format === 'percentage'
+                  ? formatPercentage(Number(item.value), 0)
+                  : item.value}
             </span>
           </div>
         ))}
