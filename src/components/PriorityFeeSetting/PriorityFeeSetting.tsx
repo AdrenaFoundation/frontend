bf1fb56bd0b23d0018e48b6e8c29c55a94874cf9
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-10-21 17:32:13 +0200

    Update PriorityFeeSetting.tsx

--- src/components/PriorityFeeSetting/PriorityFeeSetting.tsx
+++ src/components/PriorityFeeSetting/PriorityFeeSetting.tsx
@@ -95,6 +95,12 @@ export default function PriorityFeeSetting({
           )}
         >
           Now @ {formatNumber(currentPriorityFeeValue, 0)} μLamport / CU
+          <InfoAnnotation
+            className="w-3 h-3"
+            text={
+              'The Medium/High/Ultra options are based on the 55th/75th/90th percentile of the current market rate.Accurate values are fetched right before each transaction.'
+            }
+          />
         </div>
 
         <div className="mt-2">
@@ -182,109 +188,6 @@ export default function PriorityFeeSetting({
                 Currency
               </label>
               <span className="text-gray-500 sm:text-sm mr-2">SOL</span>
-            </div>
-          </div>
-
-          <div
-            className={twMerge(
-              'flex items-center justify-center mt-2 border-t pt-2 text-txtfade text-xs',
-            )}
-          >
-            Now @ {formatNumber(currentPriorityFeeValue, 0)} μLamport / CU
-            <InfoAnnotation
-              className="w-3 h-3"
-              text={
-                'The Medium/High/Ultra options are based on the 55th/75th/90th percentile of the current market rate.Accurate values are fetched right before each transaction.'
-              }
-            />
-          </div>
-
-          <div className="mt-2">
-            <div className="w-full flex flex-col border p-2 bg-third">
-              <div className="flex w-full">
-                <div className="w-1/2 items-center justify-center flex text-xs font-boldy">
-                  TX Size
-                </div>
-                <div className="w-1/2 items-center justify-center flex text-xs font-boldy">
-                  Extra Fee
-                </div>
-              </div>
-
-              <div className="flex flex-col w-full mt-1">
-                <div className="flex w-full text-xs">
-                  <div className="w-1/2 items-center justify-center flex text-txtfade">
-                    Small (200,000 cu)
-                  </div>
-                  <div className="w-1/2 items-center justify-center flex text-txtfade">
-                    {formatNumber(
-                      (200000 * currentPriorityFeeValue) / 1000000 / 1000000000,
-                      SOL_DECIMALS,
-                    )}{' '}
-                    SOL
-                  </div>
-                </div>
-
-                <div className="flex w-full text-xs">
-                  <div className="w-1/2 items-center justify-center flex text-txtfade">
-                    Average (400,000 cu)
-                  </div>
-                  <div className="w-1/2 items-center justify-center flex text-txtfade">
-                    {formatNumber(
-                      (400000 * currentPriorityFeeValue) / 1000000 / 1000000000,
-                      SOL_DECIMALS,
-                    )}{' '}
-                    SOL
-                  </div>
-                </div>
-
-                <div className="flex w-full text-xs">
-                  <div className="w-1/2 items-center justify-center flex text-txtfade">
-                    Big (700,000 cu)
-                  </div>
-                  <div className="w-1/2 items-center justify-center flex text-txtfade">
-                    {formatNumber(
-                      (700000 * currentPriorityFeeValue) / 1000000 / 1000000000,
-                      SOL_DECIMALS,
-                    )}{' '}
-                    SOL
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-4">
-            <div
-              className={twMerge(
-                'flex items-center justify-center mt-2 border-t pt-2 text-txtfade text-xs',
-              )}
-            >
-              Max Priority Fee per TX (SOL)
-              <InfoAnnotation
-                className="w-3 h-3"
-                text={
-                  'Maximum amount of SOL to be spent on priority fees per transaction, this ensure you never go over your limit.'
-                }
-              />
-            </div>
-
-            <div className="mt-1 relative rounded-md shadow-sm">
-              <input
-                type="number"
-                name="maxPriorityFee"
-                id="maxPriorityFee"
-                className="focus:ring-primary focus:border-primary block w-full pl-2 pr-12 sm:text-sm border-gray-300 rounded-md bg-third"
-                placeholder={DEFAULT_MAX_PRIORITY_FEE.toString()}
-                step="0.000000001"
-                min="0.000000001"
-                value={maxPriorityFee ?? ''}
-                onChange={(e) => setMaxPriorityFee(parseFloat(e.target.value))}
-              />
-              <div className="absolute inset-y-0 right-0 flex items-center">
-                <label htmlFor="currency" className="sr-only">
-                  Currency
-                </label>
-                <span className="text-gray-500 sm:text-sm mr-2">SOL</span>
-              </div>
             </div>
           </div>
         </div>
