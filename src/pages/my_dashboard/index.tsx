92a04c28f479f651f95d5715af077df2e2bceb52
Corto <adrena.corto@gmail.com>
2024-10-23 19:28:42 +0800

    Fix import

--- src/pages/my_dashboard/index.tsx
+++ src/pages/my_dashboard/index.tsx
@@ -15,7 +15,7 @@ import ProfileCreation from '@/components/pages/user_profile/ProfileCreation';
 import StakesStats from '@/components/pages/user_profile/StakesStats';
 import SwapStats from '@/components/pages/user_profile/SwapStats';
 import TradingStats from '@/components/pages/user_profile/TradingStats';
-import VestStats from '@/components/pages/user_profile/VestStats';
+import VestStats from '@/components/pages/user_profile/Veststats';
 import RiveAnimation from '@/components/RiveAnimation/RiveAnimation';
 import WalletConnection from '@/components/WalletAdapter/WalletConnection';
 import useWalletStakingAccounts from '@/hooks/useWalletStakingAccounts';
