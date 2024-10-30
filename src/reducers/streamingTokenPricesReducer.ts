0a6e342961b0f37a97570b4eff676f8fa8f8b4a6
Venom <184024755+0xcryptovenom@users.noreply.github.com>
2024-10-30 11:45:18 +0100

    contrib: fix streamingTokenPricesReducer should not update state unnecessarily
    Return previous state, do not unnecessarily update state
    if the new price is the same as the previous price.
    An upgrade of the Redux toolchain would help removing this boilerplate.

--- src/reducers/streamingTokenPricesReducer.ts
+++ src/reducers/streamingTokenPricesReducer.ts
@@ -11,11 +11,19 @@ export default function streamingTokenPricesReducer(
   action: StreamingTokenPricesAction,
 ) {
   switch (action.type) {
-    case 'setStreamingTokenPrice':
+    case 'setStreamingTokenPrice': {
+      // Return previous state, do not unnecessarily update state
+      // if the new price is the same as the previous price.
+      // An upgrade of the Redux toolchain would help removing this boilerplate.
+      if (state[action.payload.tokenSymbol] === action.payload.price) {
+        return state;
+      }
+
       return {
         ...state,
         [action.payload.tokenSymbol]: action.payload.price,
       };
+    }
     default:
       return state;
   }
