9cb331d58a8db8bbedc81fe2e7e8ce3a791a71cc
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-11-03 18:44:47 +0100

    banner warning

--- src/app/components/ViewsWarning/ViewsWarning.tsx
+++ src/app/components/ViewsWarning/ViewsWarning.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 import { useSelector } from '@/store/store';
 
+import crossIcon from '../../../../public/images/Icons/cross.svg';
 import infoIcon from '../../../../public/images/Icons/info.svg';
 
 export default function ViewsWarning() {
+    const isViewsWarningClosed = localStorage.getItem('viewsWarningClosed');
+
+    const wallet = useSelector((state) => state.walletState.wallet);
     const walletTokenBalances = useSelector((state) => state.walletTokenBalances);
+    const connected = !!wallet;
 
     // Ensure walletTokenBalances is loaded and wallet is connected
     if (!walletTokenBalances || !connected || isClosed) return null;
@@ -20,10 +25,19 @@ export default function ViewsWarning() {
             <div className="flex flex-row items-center gap-2">
                 <Image src={infoIcon} alt="Warning" width={14} height={14} />
 
-                <p className="font-mono font-bold">
-                    Your SOL balance is low. Please top up your wallet to get the best experience.
+                <p className="font-mono font-bold max-w-[300px] sm:max-w-max">
+                    You need at least 0.01 SOL to interact with the app
                 </p>
             </div>
+
+            <Image
+                src={crossIcon}
+                className="cursor-pointer opacity-70 hover:opacity-100 transition-opacity duration-300"
+                alt="close btn"
+                width={16}
+                height={16}
+                onClick={() => localStorage.setItem('viewsWarningClosed', 'true')}
+            />
         </div>
     );
 }
--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -142,7 +142,6 @@ export default function RootLayout({
 
       <Footer className="z-10" />
 
-      <ViewsWarning />
 
       <div className="absolute top-0 right-0 overflow-hidden w-full">
         <div id="modal-container"></div>
