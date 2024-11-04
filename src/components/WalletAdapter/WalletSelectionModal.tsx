05ec9030ed31cdc30c628a327cfc3a6d34ca627f
Orex <adrena.orex@gmail.com>
2024-11-04 19:44:14 +0400

    save

--- src/components/WalletAdapter/WalletSelectionModal.tsx
+++ src/components/WalletAdapter/WalletSelectionModal.tsx
@@ -1,7 +1,7 @@
 import { WalletReadyState } from '@solana/wallet-adapter-base';
 import { AnimatePresence } from 'framer-motion';
 import Image, { StaticImageData } from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 import {
@@ -10,6 +10,7 @@ import {
 } from '@/actions/walletActions';
 import { walletAdapters } from '@/constant';
 import { useDispatch, useSelector } from '@/store/store';
+import { WalletAdapterName } from '@/types';
 
 import Modal from '../common/Modal/Modal';
 import { WALLET_ICONS } from './WalletAdapter';
@@ -18,17 +19,32 @@ export default function WalletSelectionModal() {
   const dispatch = useDispatch();
   const { modalIsOpen } = useSelector((s) => s.walletState);
 
+  const lastConnectedWalletRef = useRef<null | WalletAdapterName>(null);
+
+  if (lastConnectedWalletRef.current === null) {
+    const adapterName = localStorage.getItem('lastConnectedWallet');
+
+    if (adapterName && adapterName in walletAdapters) {
+      lastConnectedWalletRef.current = adapterName as WalletAdapterName;
+    } else {
+      lastConnectedWalletRef.current = null;
+    }
+  }
+
   return (
     <AnimatePresence>
       {modalIsOpen && (
         <Modal
           close={() => dispatch(openCloseConnectionModalAction(false))}
-          className="flex flex-col w-full p-5 relative overflow-visible"
+          className="flex flex-col w-full relative overflow-visible"
           title="Pick a wallet"
         >
-          <div className="flex flex-col justify-center items-center gap-3">
+          <div className="flex flex-col w-[25em]">
             <WalletBlock
               name="Phantom"
+              bgColor="#ab9ff2"
+              lastConnected={lastConnectedWalletRef.current === 'phantom'}
+              imgClassName='top-[5.3em] relative'
               logo={WALLET_ICONS.phantom}
               onClick={() => {
                 dispatch(connectWalletAction('phantom'));
@@ -39,6 +55,9 @@ export default function WalletSelectionModal() {
 
             <WalletBlock
               name="Coinbase"
+              bgColor="#072b79"
+              lastConnected={lastConnectedWalletRef.current === 'coinbase'}
+              imgClassName='w-[9em] top-[5.3em] right-[2em] relative'
               logo={WALLET_ICONS.coinbase}
               onClick={() => {
                 dispatch(connectWalletAction('coinbase'));
@@ -49,6 +68,9 @@ export default function WalletSelectionModal() {
 
             <WalletBlock
               name="Solflare"
+              bgColor="#fda518"
+              lastConnected={lastConnectedWalletRef.current === 'solflare'}
+              imgClassName='w-[9em] top-[5.3em] right-[2em] relative'
               logo={WALLET_ICONS.solflare}
               onClick={() => {
                 dispatch(connectWalletAction('solflare'));
@@ -59,6 +81,9 @@ export default function WalletSelectionModal() {
 
             <WalletBlock
               name="WalletConnect"
+              bgColor="#0798fe"
+              lastConnected={lastConnectedWalletRef.current === 'walletconnect'}
+              imgClassName='top-[2em] w-[10em] right-[1em] relative'
               logo={WALLET_ICONS.walletconnect}
               onClick={() => {
                 dispatch(connectWalletAction('walletconnect'));
@@ -76,69 +101,59 @@ export default function WalletSelectionModal() {
 const WalletBlock = ({
   name,
   logo,
+  lastConnected,
   onClick,
-
+  imgClassName,
+  bgColor,
   readyState,
   className,
 }: {
   name: string;
   logo: StaticImageData;
+  lastConnected: boolean;
+  imgClassName?: string;
   onClick: () => void;
-
+  bgColor: string;
   readyState: WalletReadyState;
   className?: string;
 }) => {
   const disabled = readyState !== WalletReadyState.Installed;
 
-  const walletBlock = (
-    <div
-      className={twMerge(
-        'flex flex-row gap-3 items-center justify-between p-3 border rounded-lg w-[300px] h-[50px]',
-        disabled
-          ? 'cursor-not-allowed opacity-40'
-          : 'cursor-pointer hover:bg-bcolor duration-300',
-        className,
-      )}
-      onClick={() => {
-        if (disabled) return;
+  const [isHovered, setIsHovered] = React.useState(false);
 
-        onClick();
-      }}
+  const walletBlock = (
+    <div className={twMerge(
+      'flex w-full h-[5em] relative overflow-hidden',
+      className,
+    )}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="flex flex-row items-center gap-3">
-        <Image src={logo} alt={`${name} icon`} className="w-[30px]" />
-        <p className="text-base font-semibold">{name}</p>
+      <div
+        className={twMerge('absolute top-0 left-0 w-full h-full flex items-end justify-end', isHovered ? 'opacity-80 grayscale-0' : 'opacity-10 grayscale')}
+        style={{
+          backgroundColor: bgColor,
+        }}
+      >
+        <Image src={logo} alt={`${name} icon`} className={twMerge("w-[12em] h-auto", imgClassName)} />
       </div>
 
       <div
         className={twMerge(
-          'px-2 py-1 rounded-md',
-          readyState === WalletReadyState.Installed && 'bg-green/10',
-          readyState === WalletReadyState.NotDetected && 'bg-orange/10',
-          readyState === WalletReadyState.Unsupported && 'bg-red/10',
-          readyState === WalletReadyState.Loadable && 'bg-blue-400/10',
+          'flex p-3 w-full h-full relative items-center',
+          disabled
+            ? 'cursor-not-allowed opacity-40'
+            : 'cursor-pointer duration-300',
         )}
+        onClick={() => {
+          if (disabled) return;
+
+          onClick();
+        }}
       >
-        <p
-          className={twMerge(
-            'text-xs font-semibold font-mono',
-            readyState === WalletReadyState.Installed && 'text-green',
-            readyState === WalletReadyState.NotDetected && 'text-orange',
-            readyState === WalletReadyState.Unsupported && 'text-red',
-            readyState === WalletReadyState.Loadable && 'text-blue-400',
-          )}
-        >
-          {(() => {
-            return (
-              {
-                [WalletReadyState.NotDetected]: 'Not installed',
-                [WalletReadyState.Loadable]: 'Not loaded yet',
-                [WalletReadyState.Unsupported]: 'Not supported',
-                [WalletReadyState.Installed]: 'Installed',
-              }[readyState.toString()] ?? 'Cannot connect'
-            );
-          })()}
-        </p>
+        <div className="text-lg font-boldy flex items-center ml-8 pt-1 pb-1 pl-3 pr-3 bg-main">{name}</div>
+
+        {lastConnected ? <div className={twMerge('flex text-sm ml-4 italic', isHovered ? 'text-white' : 'text-txtfade')}>Last Pick!</div> : null}
       </div>
     </div>
   );
