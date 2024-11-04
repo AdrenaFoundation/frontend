9f46796353e5e37e9358cf71c77ccce36cbc3b46
Orex <122088118+adrena-orex@users.noreply.github.com>
2024-11-04 09:10:23 +0400

    Fix number max/min digit display (#405)

--- src/utils.tsx
+++ src/utils.tsx
@@ -89,8 +89,8 @@ export function formatNumber(
   minimumFractionDigits = 0,
   precisionIfPriceDecimalsBelow = 6,
 ): string {
-  // If price is below decimals precision, display up to 6 decimals
-  if (nb < 10 ** -precision) precision = precisionIfPriceDecimalsBelow;
+  // If price is below decimals precision, display up to 6 decimals (override by minimumFractionDigits)
+  if (nb < 10 ** -precision) precision = Math.max(precisionIfPriceDecimalsBelow, minimumFractionDigits);
 
   return Number(nb.toFixed(precision)).toLocaleString(undefined, {
     minimumFractionDigits,
