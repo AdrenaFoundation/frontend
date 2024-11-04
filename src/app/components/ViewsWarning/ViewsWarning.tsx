c29fb68e2b7ef11e87c3cf1ad1b976f418c942a4
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-11-04 11:18:59 +0100

    Update ViewsWarning.tsx

--- src/app/components/ViewsWarning/ViewsWarning.tsx
+++ src/app/components/ViewsWarning/ViewsWarning.tsx
@@ -8,7 +8,6 @@ import infoIcon from '../../../../public/images/Icons/info.svg';
 
 export default function ViewsWarning() {
     const [isClosed, setIsClosed] = useState(false);
-    const isViewsWarningClosed = localStorage.getItem('viewsWarningClosed');
 
     const wallet = useSelector((state) => state.walletState.wallet);
     const walletTokenBalances = useSelector((state) => state.walletTokenBalances);
@@ -16,21 +15,15 @@ export default function ViewsWarning() {
 
     const solBalance = walletTokenBalances?.['SOL'] ?? 0;
 
-    if (
-        solBalance > 0.01 ||
-        !connected ||
-        isClosed ||
-        isViewsWarningClosed === 'true'
-    )
-        return null;
+    if (solBalance > 0.001 || !connected || isClosed) return null;
 
     return (
-        <div className="flex flex-row items-center justify-between p-3 bg-amber-600 w-full z-30">
+        <div className="flex flex-row items-center justify-center gap-3 p-1 bg-amber-700 w-full z-30">
             <div className="flex flex-row items-center gap-2">
-                <Image src={infoIcon} alt="Warning" width={16} height={16} />
+                <Image src={infoIcon} alt="Warning" width={14} height={14} />
 
-                <p className="font-mono font-bold max-w-[300px] sm:max-w-max">
-                    You need at least 0.01 SOL to interact with the app
+                <p className="text-sm font-boldy max-w-[300px] sm:max-w-max text-center">
+                    You need at least 0.001 SOL to interact with the app
                 </p>
             </div>
 
@@ -38,11 +31,10 @@ export default function ViewsWarning() {
                 src={crossIcon}
                 className="cursor-pointer opacity-70 hover:opacity-100 transition-opacity duration-300"
                 alt="close btn"
-                width={16}
-                height={16}
+                width={14}
+                height={14}
                 onClick={() => {
                     setIsClosed(true);
-                    localStorage.setItem('viewsWarningClosed', 'true');
                 }}
             />
         </div>
