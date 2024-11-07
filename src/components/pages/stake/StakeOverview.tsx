4b2607b527e3511262038bcb1cb17da24b45770b
Vince <31858345+vsusini@users.noreply.github.com>
2024-11-07 17:13:03 -0500

    ty jaki

--- src/components/pages/stake/StakeOverview.tsx
+++ src/components/pages/stake/StakeOverview.tsx
@@ -245,7 +245,7 @@ export default function StakeOverview({
         <div className="flex flex-col sm:flex-row items-stretch h-full w-full bg-gradient-to-br from-[#07111A] to-[#0B1722] border rounded-lg shadow-lg">
           <div
             className={twMerge(
-              'flex items-center w-full sm:w-auto sm:min-w-[200px] rounded-t-lg sm:rounded-r-none sm:rounded-l-lg p-3 flex-none sm:border-r flex-grow',
+              'flex items-center w-full sm:w-auto sm:min-w-[200px] rounded-t-lg sm:rounded-r-none sm:rounded-l-lg p-3 flex-none sm:border-r',
               isALP ? 'bg-[#130AAA]' : 'bg-[#991B1B]',
             )}
           >
