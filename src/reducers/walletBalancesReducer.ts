d210c46f7315513749bbf446147b1b326fffb26b
Venom <184024755+0xcryptovenom@users.noreply.github.com>
2024-11-09 16:18:36 +0100

    contrib: fix wallet token balances should reset when user disconnects

--- src/reducers/walletBalancesReducer.ts
+++ src/reducers/walletBalancesReducer.ts
@@ -1,6 +1,10 @@
-import { TokenSymbol } from '@/types';
+import type { DisconnectWalletAction } from '@/actions/walletActions';
+import type { TokenSymbol } from '@/types';
 
-import type { WalletBalancesActions } from '../actions/walletBalances';
+import {
+  SET_TOKEN_BALANCES_ACTION_TYPE,
+  type WalletBalancesActions,
+} from '../actions/walletBalances';
 
 export type WalletBalancesState = Record<TokenSymbol, number | null> | null;
 
@@ -8,11 +12,15 @@ const initialState: WalletBalancesState = null;
 
 export default function walletReducer(
   state = initialState,
-  action: WalletBalancesActions,
+  action: WalletBalancesActions | DisconnectWalletAction,
 ) {
   switch (action.type) {
-    case 'setTokenBalances':
+    case SET_TOKEN_BALANCES_ACTION_TYPE:
       return action.payload;
+    // reset wallet balances state immediately
+    // when the user disconnects their wallet.
+    case 'disconnect':
+      return initialState;
     default:
       return state;
   }
