fa8198324f52800df319259b43df9b3ab13d4688
Orex <122088118+adrena-orex@users.noreply.github.com>
2024-11-10 11:13:22 +0400

    All positions chart in monitoring (#446)

--- src/hooks/useAllPositions.tsx
+++ src/hooks/useAllPositions.tsx
@@ -54,7 +54,6 @@ export function useAllPositions({ connected }: { connected: boolean }): {
                 setIsLoading(false);
             } catch (e) {
                 console.log('Error loading positions', e, String(e));
-
                 // Do nothing
             }
 
@@ -70,7 +69,7 @@ export function useAllPositions({ connected }: { connected: boolean }): {
             calculatePnLandLiquidationPrice(position, tokenPrices);
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [tokenPrices]);
+    }, [tokenPrices, allPositions]);
 
     useEffect(() => {
         loadAllPositions();
@@ -78,7 +77,6 @@ export function useAllPositions({ connected }: { connected: boolean }): {
         const interval = setInterval(async () => {
             await loadAllPositions();
         }, 60000);
-
 
         return () => {
             clearInterval(interval);
