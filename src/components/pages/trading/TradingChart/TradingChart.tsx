f52164d85a994fb1123f0204aea12883eb1e3cdb
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2024-12-02 22:07:48 +0100

    chart drawing refacto

--- src/components/pages/trading/TradingChart/TradingChart.tsx
+++ src/components/pages/trading/TradingChart/TradingChart.tsx
@@ -1,4 +1,3 @@
-import { PublicKey } from '@solana/web3.js';
 import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
@@ -10,160 +9,17 @@ import { formatNumber, formatNumberShort, getTokenSymbol } from '@/utils';
 import {
   EntityId,
   IChartingLibraryWidget,
-  IChartWidgetApi,
-  IPositionLineAdapter,
   ISymbolValueFormatter,
-  PricedPoint,
   ResolutionString,
   SupportedLineTools,
 } from '../../../../../public/charting_library/charting_library';
 import datafeed from './datafeed';
+import { useChartDrawing } from '@/hooks/useChartDrawing';
+import { useSelector } from '@/store/store';
 
 let tvScriptLoadingPromise: Promise<unknown>;
 
 type Widget = IChartingLibraryWidget;
-const greenColor = '#07956be6';
-const redColor = '#c9243ae6';
-const greyColor = '#78828e';
-const whiteColor = '#ffffff';
-const orangeColor = '#f77f00';
-const blueColor = '#3a86ff';
-const purpleColor = '#9333ea';
-
-function createEntryPositionLine(
-  chart: IChartWidgetApi,
-  position: PositionExtended,
-): IPositionLineAdapter {
-  const color = position.side === 'long' ? greenColor : redColor;
-  const label = getLabel(
-    position.token.symbol,
-    position.side,
-    position.side === 'long' ? 'Long' : 'Short',
-  );
-  return chart
-    .createPositionLine({})
-    .setText(label)
-    .setLineLength(3)
-    .setPrice(position.price)
-    .setLineColor(color)
-    .setBodyBackgroundColor(color)
-    .setBodyBorderColor(color)
-    .setBodyTextColor(whiteColor)
-    .setQuantity('$' + formatNumberShort(position.sizeUsd, 0))
-    .setQuantityBackgroundColor(greyColor)
-    .setQuantityBorderColor(greyColor);
-}
-
-function createBreakEvenPositionLine(
-  chart: IChartWidgetApi,
-  position: PositionExtended,
-  breakEvenPrice: number,
-): IPositionLineAdapter {
-  const color = `${purpleColor}80`;
-  const label = getLabel(
-    position.token.symbol,
-    position.side,
-    'Break Even',
-  );
-  return chart
-    .createPositionLine({})
-    .setText(label)
-    .setLineLength(3)
-    .setPrice(breakEvenPrice)
-    .setLineColor(color)
-    .setLineStyle(2)
-    .setBodyBackgroundColor(color)
-    .setBodyBorderColor(color)
-    .setBodyTextColor(whiteColor)
-    .setQuantity('') // Set quantity to an empty string
-    .setQuantityBackgroundColor('transparent') // Make background transparent
-    .setQuantityBorderColor('transparent'); // Make border transparent
-}
-
-function createLiquidationPositionLine(
-  chart: IChartWidgetApi,
-  position: PositionExtended,
-): IPositionLineAdapter {
-  return (
-    chart
-      .createPositionLine({})
-      .setText(`${position.side === 'long' ? 'Long' : 'Short'} Liq.`)
-      .setLineLength(3)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      .setPrice(position.liquidationPrice!) // Price is checked before calling function
-      .setLineColor(orangeColor)
-      .setLineStyle(1)
-      .setBodyBorderColor(orangeColor)
-      .setBodyBackgroundColor(orangeColor)
-      .setBodyTextColor(whiteColor)
-      .setQuantity('$' + formatNumberShort(position.sizeUsd, 0))
-      .setQuantityBackgroundColor(greyColor)
-      .setQuantityBorderColor(greyColor)
-  );
-}
-
-function getLabel(
-  tokenSymbol: string,
-  side: 'long' | 'short',
-  type: 'SL' | 'TP' | 'Long' | 'Short' | 'Break Even',
-): string {
-  const symbol = getTokenSymbol(tokenSymbol);
-
-  // Return the label for the position line
-  if (type === 'Long' || type === 'Short' || type === 'Break Even') {
-    return `${symbol} ${type}`;
-  }
-
-  // Return the label for the stop loss or take profit line
-  const sideLabel = side === 'long' ? '' : '-Short';
-  return `${symbol}${sideLabel}-${type}`;
-}
-
-function createTakeProfitPositionLine(
-  chart: IChartWidgetApi,
-  position: PositionExtended,
-): IPositionLineAdapter {
-  const label = getLabel(position.token.symbol, position.side, 'TP');
-  return (
-    chart
-      .createPositionLine({})
-      .setText(label)
-      .setLineLength(3)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      .setPrice(position.takeProfitLimitPrice!)
-      .setLineColor(blueColor)
-      .setLineStyle(1)
-      .setBodyBorderColor(blueColor)
-      .setBodyBackgroundColor(blueColor)
-      .setBodyTextColor(whiteColor)
-      .setQuantity('$' + formatNumberShort(position.sizeUsd, 0))
-      .setQuantityBackgroundColor(greyColor)
-      .setQuantityBorderColor(greyColor)
-  );
-}
-
-function createStopLossPositionLine(
-  chart: IChartWidgetApi,
-  position: PositionExtended,
-): IPositionLineAdapter {
-  const label = getLabel(position.token.symbol, position.side, 'SL');
-  return (
-    chart
-      .createPositionLine({})
-      .setText(label)
-      .setLineLength(3)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      .setPrice(position.stopLossLimitPrice!)
-      .setLineColor(blueColor)
-      .setLineStyle(1)
-      .setBodyBorderColor(blueColor)
-      .setBodyBackgroundColor(blueColor)
-      .setBodyTextColor(whiteColor)
-      .setQuantity('$' + formatNumberShort(position.sizeUsd, 0))
-      .setQuantityBackgroundColor(greyColor)
-      .setQuantityBorderColor(greyColor)
-  );
-}
 
 const STORAGE_KEY_RESOLUTION = 'trading_chart_resolution';
 
@@ -178,115 +34,19 @@ export default function TradingChart({
 }) {
   const onLoadScriptRef: MutableRefObject<(() => void) | null> = useRef(null);
 
-  const savedShapes = localStorage.getItem('chart_shapes');
-
-  const [drawings, setDrawings] = useState<
-    Record<
-      TokenSymbol,
-      {
-        id: EntityId;
-        name: Exclude<
-          SupportedLineTools,
-          'cursor' | 'dot' | 'arrow_cursor' | 'eraser' | 'measure' | 'zoom'
-        >;
-        points: PricedPoint[];
-        symbol: TokenSymbol;
-      }[]
-    >
-  >(savedShapes ? JSON.parse(savedShapes) : []);
   const [widget, setWidget] = useState<Widget | null>(null);
   const [widgetReady, setWidgetReady] = useState<boolean | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  type PositionLine = {
-    position: PublicKey;
-    liquidation?: IPositionLineAdapter;
-    entry: IPositionLineAdapter;
-    breakEven?: IPositionLineAdapter;
-    stopLoss?: IPositionLineAdapter;
-    takeProfit?: IPositionLineAdapter;
-  };
-
-  const [positionLines, setPositionLines] = useState<{
-    short: PositionLine | null;
-    long: PositionLine | null;
-  } | null>(null);
+  useChartDrawing({
+    widget,
+    widgetReady,
+    positions,
+    showBreakEvenLine,
+  });
 
   // Retrieve saved resolution or default to 'H'
   const savedResolution = localStorage.getItem(STORAGE_KEY_RESOLUTION) || 'H';
-
-  function modifyPositionLine(
-    chart: IChartWidgetApi,
-    position: PositionExtended | null,
-    positionLine: PositionLine | null,
-  ) {
-    if (!position || !positionLine) return;
-
-    positionLine.entry.setPrice(position.price);
-
-    // Handle Liquidation
-    if (!position.liquidationPrice) {
-      if (positionLine.liquidation) {
-        positionLine.liquidation.remove();
-        positionLine.liquidation = undefined;
-      }
-    } else {
-      if (positionLine.liquidation) {
-        positionLine.liquidation.setPrice(position.liquidationPrice);
-      } else {
-        positionLine.liquidation = createLiquidationPositionLine(
-          chart,
-          position,
-        );
-      }
-    }
-
-    // Break Even line
-    if (showBreakEvenLine) {
-      if (positionLine.breakEven) {
-        positionLine.breakEven.setPrice(position.breakEvenPrice);
-      } else {
-        positionLine.breakEven = createBreakEvenPositionLine(
-          chart,
-          position,
-          position.breakEvenPrice,
-        );
-      }
-    } else {
-      if (positionLine.breakEven) {
-        positionLine.breakEven.remove();
-        positionLine.breakEven = undefined;
-      }
-    }
-
-    // Handle Take Profit
-    if (!position.takeProfitIsSet || !position.takeProfitLimitPrice) {
-      if (positionLine.takeProfit) {
-        positionLine.takeProfit.remove();
-        positionLine.takeProfit = undefined;
-      }
-    } else {
-      if (positionLine.takeProfit) {
-        positionLine.takeProfit.setPrice(position.takeProfitLimitPrice);
-      } else {
-        positionLine.takeProfit = createTakeProfitPositionLine(chart, position);
-      }
-    }
-
-    // Handle Stop Loss
-    if (!position.stopLossIsSet || !position.stopLossLimitPrice) {
-      if (positionLine.stopLoss) {
-        positionLine.stopLoss.remove();
-        positionLine.stopLoss = undefined;
-      }
-    } else {
-      if (positionLine.stopLoss) {
-        positionLine.stopLoss.setPrice(position.stopLossLimitPrice);
-      } else {
-        positionLine.stopLoss = createStopLossPositionLine(chart, position);
-      }
-    }
-  }
 
   useEffect(() => {
     function createWidget() {
@@ -338,14 +98,13 @@ export default function TradingChart({
             'paneProperties.legendProperties.showSeriesTitle': false,
             'paneProperties.legendProperties.showBarChange': false,
             'paneProperties.legendProperties.showSeriesOHLC': true,
-            'mainSeriesProperties.priceLineColor': 'yellow',
           },
           theme: 'dark',
           interval: SUPPORTED_RESOLUTIONS.includes(
             savedResolution as ResolutionString,
           )
-            ? savedResolution as ResolutionString
-            : '1D' as ResolutionString,
+            ? (savedResolution as ResolutionString)
+            : ('1D' as ResolutionString),
           custom_formatters: {
             priceFormatterFactory: (): ISymbolValueFormatter | null => {
               return {
@@ -361,38 +120,64 @@ export default function TradingChart({
           setWidgetReady(true);
           setIsLoading(false);
 
+          widget.applyOverrides({
+            'paneProperties.background': '#171B26',
+            'paneProperties.backgroundType': 'solid',
+            'paneProperties.legendProperties.showStudyArguments': false,
+            'paneProperties.legendProperties.showStudyTitles': false,
+            'paneProperties.legendProperties.showStudyValues': false,
+            'paneProperties.legendProperties.showSeriesTitle': false,
+            'paneProperties.legendProperties.showBarChange': false,
+            'paneProperties.legendProperties.showSeriesOHLC': true,
+            'mainSeriesProperties.priceLineColor': '#FFFF05',
+          });
+
           widget.subscribe('drawing_event', () => {
-            const allShapes = widget.activeChart().getAllShapes();
             const symbol = widget
               .activeChart()
               .symbol()
               .split('.')[1]
               .split('/')[0] as TokenSymbol;
+            const parsedChartShapes = JSON.parse(
+              localStorage.getItem('chart_drawings') ?? '{}',
+            );
 
-            const copiedDrawings = { ...drawings };
+            const userDrawings = widget
+              .activeChart()
+              .getAllShapes()
+              .map((line) => {
+                const points = widget
+                  .activeChart()
+                  .getShapeById(line.id)
+                  .getPoints();
 
-            copiedDrawings[symbol] = allShapes.map((shape) => {
-              return {
-                id: shape.id,
-                name: shape.name as Exclude<
-                  SupportedLineTools,
-                  | 'cursor'
-                  | 'dot'
-                  | 'arrow_cursor'
-                  | 'eraser'
-                  | 'measure'
-                  | 'zoom'
-                >,
-                points: widget.activeChart().getShapeById(shape.id).getPoints(),
-                symbol,
-              };
-            });
+                const shape = widget
+                  .activeChart()
+                  .getShapeById(line.id)
+                  .getProperties();
 
-            setDrawings(copiedDrawings);
+                if (
+                  !(
+                    shape?.text?.includes('long') ||
+                    shape?.text?.includes('short')
+                  ) // TODO: revisit and refactor
+                ) {
+                  return {
+                    id: line.id as EntityId,
+                    points,
+                    name: line.name as SupportedLineTools,
+                    options: shape,
+                  };
+                }
+              })
+              .filter((line) => line);
 
             localStorage.setItem(
-              'chart_shapes',
-              JSON.stringify(copiedDrawings),
+              'chart_drawings',
+              JSON.stringify({
+                ...parsedChartShapes,
+                [symbol]: userDrawings,
+              }),
             );
           });
 
@@ -437,50 +222,13 @@ export default function TradingChart({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    // add drawings if any
-    if (widget && widgetReady) {
-      const symbol = widget
-        .activeChart()
-        .symbol()
-        .split('.')[1]
-        .split('/')[0] as TokenSymbol;
-
-      const savedDrawings = drawings[symbol];
-      const currentShapes = widget
-        .activeChart()
-        .getAllShapes()
-        .map(({ id }) => id);
-
-      if (savedDrawings && savedDrawings.length > 0) {
-        savedDrawings.forEach(({ id, name, points }) => {
-          if (points.length === 0) {
-            localStorage.setItem(
-              'chart_shapes',
-              JSON.stringify({
-                ...drawings,
-                [symbol]: [],
-              }),
-            );
-            return;
-          }
-
-          if (currentShapes.includes(id)) return;
-
-          widget.activeChart().createMultipointShape(points, {
-            shape: name,
-          });
-        });
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [widgetReady]);
-
   // When the token changes, we need to change the symbol of the widget so we only reload the chart
   useEffect(() => {
+    if (!widget) return;
+
     setWidgetReady(false);
 
-    widget?.setSymbol(
+    widget.setSymbol(
       `Crypto.${getTokenSymbol(token.symbol)}/USD`,
       savedResolution as ResolutionString,
       () => {
@@ -488,139 +236,8 @@ export default function TradingChart({
       },
     );
 
-    // delete all positions on chart
-    positionLines?.long?.entry.remove();
-    positionLines?.short?.entry.remove();
-    positionLines?.long?.liquidation?.remove();
-    positionLines?.short?.liquidation?.remove();
-    positionLines?.long?.takeProfit?.remove();
-    positionLines?.short?.takeProfit?.remove();
-    positionLines?.long?.stopLoss?.remove();
-    positionLines?.short?.stopLoss?.remove();
-
-    setPositionLines(null);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token.symbol]);
-
-  useEffect(() => {
-    try {
-      if (!widget || !widgetReady) return;
-
-      const chart: IChartWidgetApi = widget.activeChart();
-
-      const newPositionLines = positionLines ?? {
-        short: null,
-        long: null,
-      };
-
-      if (positions) {
-        const longPosition =
-          positions.find(
-            (position) =>
-              position.token.symbol === token.symbol &&
-              position.side === 'long',
-          ) ?? null;
-
-        const shortPosition =
-          positions.find(
-            (position) =>
-              position.token.symbol === token.symbol &&
-              position.side === 'short',
-          ) ?? null;
-
-        // handle creation / modification / deletion for long positions
-        if (longPosition) {
-          if (newPositionLines && newPositionLines?.long) {
-            modifyPositionLine(chart, longPosition, newPositionLines.long);
-          } else {
-            newPositionLines.long = {
-              entry: createEntryPositionLine(chart, longPosition),
-              breakEven: showBreakEvenLine && longPosition.breakEvenPrice
-                ? createBreakEvenPositionLine(chart, longPosition, longPosition.breakEvenPrice)
-                : undefined,
-              liquidation: longPosition.liquidationPrice
-                ? createLiquidationPositionLine(chart, longPosition)
-                : undefined,
-              position: longPosition.pubkey,
-              takeProfit:
-                longPosition.takeProfitIsSet &&
-                  longPosition.takeProfitLimitPrice &&
-                  longPosition.takeProfitLimitPrice > 0
-                  ? createTakeProfitPositionLine(chart, longPosition)
-                  : undefined,
-              stopLoss:
-                longPosition.stopLossIsSet &&
-                  longPosition.stopLossLimitPrice &&
-                  longPosition.stopLossLimitPrice > 0
-                  ? createStopLossPositionLine(chart, longPosition)
-                  : undefined,
-            };
-          }
-        } else if (newPositionLines.long) {
-          newPositionLines.long.entry.remove();
-
-          if (newPositionLines.long.liquidation)
-            newPositionLines.long.liquidation.remove();
-          if (newPositionLines.long.takeProfit)
-            newPositionLines.long.takeProfit.remove();
-          if (newPositionLines.long.stopLoss)
-            newPositionLines.long.stopLoss.remove();
-
-          newPositionLines.long = null;
-        }
-
-        // handle creation / modification / deletion for short positions
-        if (shortPosition) {
-          if (newPositionLines && newPositionLines?.short) {
-            modifyPositionLine(chart, shortPosition, newPositionLines.short);
-          } else {
-            newPositionLines.short = {
-              entry: createEntryPositionLine(chart, shortPosition),
-              liquidation: shortPosition.liquidationPrice
-                ? createLiquidationPositionLine(chart, shortPosition)
-                : undefined,
-              position: shortPosition.pubkey,
-              takeProfit:
-                shortPosition.takeProfitIsSet &&
-                  shortPosition.takeProfitLimitPrice &&
-                  shortPosition.takeProfitLimitPrice > 0
-                  ? createTakeProfitPositionLine(chart, shortPosition)
-                  : undefined,
-              stopLoss:
-                shortPosition.stopLossIsSet &&
-                  shortPosition.stopLossLimitPrice &&
-                  shortPosition.stopLossLimitPrice > 0
-                  ? createStopLossPositionLine(chart, shortPosition)
-                  : undefined,
-            };
-          }
-        } else if (newPositionLines.short) {
-          newPositionLines.short.entry.remove();
-
-          if (newPositionLines.short.liquidation)
-            newPositionLines.short.liquidation.remove();
-          if (newPositionLines.short.takeProfit)
-            newPositionLines.short.takeProfit.remove();
-          if (newPositionLines.short.stopLoss)
-            newPositionLines.short.stopLoss.remove();
-
-          newPositionLines.short = null;
-        }
-      }
-
-      setPositionLines(newPositionLines);
-    } catch {
-      // ignore error due to conflicts with charts and react effects
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    positions,
-    token.symbol,
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    !!widget,
-    widgetReady,
-    showBreakEvenLine,
-  ]);
 
   return (
     <div className="flex flex-col w-full overflow-hidden bg-secondary select-none">
---
+++ src/hooks/useChartDrawing.ts
@@ -0,0 +1,242 @@
+import { PositionExtended, TokenSymbol } from '@/types';
+import { useCallback, useEffect, useState } from 'react';
+import {
+  EntityId,
+  IChartingLibraryWidget,
+  PricedPoint,
+  SupportedLineTools,
+} from '../../public/charting_library/charting_library';
+import { formatNumberShort, getTokenSymbol } from '@/utils';
+import {
+  blueColor,
+  greenColor,
+  orangeColor,
+  purpleColor,
+  redColor,
+} from '@/constant';
+
+export function useChartDrawing({
+  widget,
+  widgetReady,
+  positions,
+  showBreakEvenLine,
+}: {
+  widget: IChartingLibraryWidget | null;
+  widgetReady: boolean | null;
+  positions: PositionExtended[] | null;
+  showBreakEvenLine: boolean;
+}) {
+  useEffect(() => {
+    if (!widgetReady || !widget) return;
+
+    const symbol = widget
+      .activeChart()
+      .symbol()
+      .split('.')[1]
+      .split('/')[0] as TokenSymbol;
+    const parsedChartShapes = JSON.parse(
+      localStorage.getItem('chart_shapes') ?? '{}',
+    );
+
+    const currentShapes = widget.activeChart().getAllShapes();
+    const currentShapesIds = currentShapes.map((shape) => shape.id);
+
+    try {
+      if (parsedChartShapes[symbol]) {
+        console.log('drawings', parsedChartShapes[symbol]);
+        parsedChartShapes[symbol].forEach((shape: any) => {
+          if (!currentShapesIds.includes(shape.id)) {
+            widget.activeChart().createMultipointShape(shape.points, {
+              zOrder: 'top',
+              shape: shape.name,
+              showInObjectsTree: true,
+              overrides: {
+                ...shape.options,
+              },
+              text: shape.options.text,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      console.error('error', error);
+
+      localStorage.setItem(
+        'chart_drawings',
+        JSON.stringify({ ...parsedChartShapes, [symbol]: [] }),
+      );
+    }
+  }, [widgetReady, widget]);
+
+  // handles position lines
+  useEffect(() => {
+    if (!widgetReady || !widget || !positions) return;
+    widget
+      .activeChart()
+      .getAllShapes()
+      .filter((shape) => shape.name === 'horizontal_line')
+      .forEach((line) => {
+        const shape = widget
+          .activeChart()
+          .getShapeById(line.id)
+          .getProperties();
+
+        // TODO: revisit and refactor
+        if (shape.text.includes('long') || shape.text.includes('short')) {
+          widget.activeChart().removeEntity(line.id);
+        }
+      });
+
+    positions.forEach((position) => {
+      addHorizontalLine({
+        id: `${position.token.symbol}-${position.side}` as EntityId,
+        text: `${getTokenSymbol(position.token.symbol)} ${
+          position.side
+        }: $${formatNumberShort(position.sizeUsd, 0)}`,
+        price: position.price,
+        time: new Date(Number(position.nativeObject.openTime) * 1000),
+        color: position.side === 'long' ? greenColor : redColor,
+        linestyle: 0,
+        linewidth: 2,
+      });
+
+      if (position?.liquidationPrice) {
+        addHorizontalLine({
+          id: `liq-${position.token.symbol}-${position.side}` as EntityId,
+          text: `${position.side} – liq: $${formatNumberShort(
+            position.sizeUsd,
+            0,
+          )}`,
+          price: position.liquidationPrice,
+          time: new Date(Number(position.nativeObject.openTime) * 1000),
+          color: orangeColor,
+          linestyle: 1,
+          linewidth: 1,
+        });
+      }
+
+      if (
+        position.takeProfitIsSet &&
+        position?.takeProfitLimitPrice &&
+        position.takeProfitLimitPrice > 0
+      ) {
+        addHorizontalLine({
+          id: `tp-${position.token.symbol}-${position.side}` as EntityId,
+          text: `${position.side} – TP: $${formatNumberShort(
+            position.sizeUsd,
+            0,
+          )}`,
+          price: position.takeProfitLimitPrice,
+          time: new Date(Number(position.nativeObject.openTime) * 1000),
+          color: blueColor,
+          linestyle: 1,
+          linewidth: 1,
+        });
+      }
+
+      if (
+        position.stopLossIsSet &&
+        position?.stopLossLimitPrice &&
+        position.stopLossLimitPrice > 0
+      ) {
+        addHorizontalLine({
+          id: `sl-${position.token.symbol}-${position.side}` as EntityId,
+          text: `${position.side} – SL: $${formatNumberShort(
+            position.sizeUsd,
+            0,
+          )}`,
+          price: position.stopLossLimitPrice,
+          time: new Date(Number(position.nativeObject.openTime) * 1000),
+          color: blueColor,
+          linestyle: 1,
+          linewidth: 1,
+        });
+      }
+    });
+  }, [widgetReady, positions]);
+
+  // handle break even lines
+  useEffect(() => {
+    if (!widgetReady || !widget || !positions) return;
+
+    positions.forEach((position) => {
+      if (showBreakEvenLine) {
+        addHorizontalLine({
+          id: `be-${position.token.symbol}-${position.side}` as EntityId,
+          text: `${getTokenSymbol(position.token.symbol)} ${getTokenSymbol(
+            position.side,
+          )} – break even`,
+          price: position.breakEvenPrice,
+          time: new Date(Number(position.nativeObject.openTime) * 1000),
+          color: `${purpleColor}80`,
+          linestyle: 2,
+          linewidth: 1,
+        });
+      } else {
+        widget
+          .activeChart()
+          .getAllShapes()
+          .filter((shape) => shape.name === 'horizontal_line')
+          .forEach((line) => {
+            const currentText = widget
+              .activeChart()
+              .getShapeById(line.id)
+              .getProperties().text;
+
+            if (currentText.includes('break even')) {
+              widget.activeChart().removeEntity(line.id);
+            }
+          });
+      }
+    });
+  }, [widgetReady, positions, showBreakEvenLine]);
+
+  const addHorizontalLine = useCallback(
+    ({
+      id, // TODO: unique id for the position lines
+      price,
+      text,
+      color,
+      time,
+      linestyle = 0,
+      linewidth = 1,
+    }: {
+      id: EntityId;
+      price: number;
+      text: string;
+      color: string;
+      time: Date;
+      linestyle?: number;
+      linewidth?: number;
+    }) => {
+      if (!widget || !widgetReady) return;
+
+      widget.activeChart().createShape(
+        {
+          time: new Date(time).getTime(),
+          price,
+        },
+        {
+          zOrder: 'top',
+          shape: 'horizontal_line',
+          lock: true,
+          disableSelection: true,
+          overrides: {
+            linestyle,
+            linewidth,
+            bold: true,
+            linecolor: color,
+            horzLabelsAlign: 'right',
+            vertLabelsAlign: 'bottom',
+            showLabel: true,
+            fontsize: 10,
+            textcolor: color,
+            showInObjectsTree: true,
+          },
+          text,
+        },
+      );
+    },
+    [widget, widgetReady],
+  );
+}
