2255564c83ed83692470e639af7c76e3d2f666bb
adrena-kino <adrena.kino@gmail.com>
2024-12-15 17:21:06 +0400

    upgrade all versions to newest possible

--- src/components/pages/user_profile/TradingStats.tsx
+++ src/components/pages/user_profile/TradingStats.tsx
@@ -23,10 +23,6 @@ export default function TradingStats({
     userProfile.longStats.feePaidUsd +
     userProfile.shortStats.feePaidUsd;
 
-  console.log('STATS', {
-    userProfile
-  })
-
   return (
     <div className={twMerge("flex-wrap flex-row w-full flex", className)}>
       <NumberDisplay
