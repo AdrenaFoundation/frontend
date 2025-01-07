c3a1d8a3b1ca5d195d9312c321916fa266cf8050
Orex <adrena.orex@gmail.com>
2025-01-07 14:36:16 +0400

    Tokenomics page

---
+++ src/components/pages/global/AllVestingChart/AllVestingChart.tsx
@@ -0,0 +1,245 @@
+import { PublicKey } from '@solana/web3.js';
+import React, { useEffect, useState } from 'react';
+import { ResponsiveContainer, Treemap } from 'recharts';
+import { twMerge } from 'tailwind-merge';
+
+import Loader from '@/components/Loader/Loader';
+import { getAccountExplorer, nativeToUi } from '@/utils';
+import { Vest, VestExtended } from '@/types';
+
+const colors = {
+  team: "#d4c7df",
+  investors: "#9e8cae",
+  foundation: "#db606c",
+} as const;
+
+function formatNum(num: number): string {
+  if (num > 999_999_999) {
+    return (num / 1_000_000_000).toFixed(2) + 'B';
+  }
+
+  if (num > 999_999) {
+    return (num / 1_000_000).toFixed(2) + 'M';
+  }
+
+  if (num > 999) {
+    return (num / 1_000).toFixed(2) + 'K';
+  }
+
+  return num.toString();
+}
+
+export const CustomizedContent: React.FC<{
+  root: unknown;
+  depth: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  index: number;
+  payload: unknown;
+  color: string;
+  rank: number;
+  name: string;
+  type?: 'team' | 'investor' | 'foundation';
+  size: number;
+  claimedAmount: number;
+  vestPubkey: PublicKey;
+}> = ({
+  depth,
+  x,
+  y,
+  width,
+  height,
+  index,
+  color,
+  name,
+  type,
+  size,
+  claimedAmount,
+  vestPubkey,
+}) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const num: string = formatNum(size);
+    const claimedAmountFormatted: string = formatNum(claimedAmount);
+
+    return (
+      <g key={`node-${index}-${depth}-${name}`} className='relative'
+        onMouseEnter={() => {
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false);
+        }}
+      >
+        <rect
+          className={twMerge(vestPubkey.toBase58() !== PublicKey.default.toBase58() ? 'cursor-pointer' : '')}
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          onClick={() => {
+            window.open(getAccountExplorer(vestPubkey), '_blank');
+          }}
+          style={{
+            fill: color,
+            stroke: "#fff",
+            strokeWidth: depth === 1 ? 5 : 1,
+            opacity: isHovered && vestPubkey.toBase58() !== PublicKey.default.toBase58() ? 1 : 0.7,
+            strokeOpacity: 1,
+          }}
+        />
+
+        {size ? <rect
+          className={twMerge(vestPubkey.toBase58() !== PublicKey.default.toBase58() ? 'cursor-pointer' : '')}
+          x={x}
+          y={y + height * (1 - claimedAmount / size)}
+          width={width}
+          height={height * (claimedAmount / size)}
+          onClick={() => {
+            window.open(getAccountExplorer(vestPubkey), '_blank');
+          }}
+          style={{
+            fill: color,
+            stroke: "#fff",
+            strokeWidth: depth === 1 ? 5 : 1,
+            opacity: isHovered && vestPubkey.toBase58() !== PublicKey.default.toBase58() ? 1 : 0.7,
+            strokeOpacity: 1,
+          }}
+        /> : null}
+
+        {
+          depth === 2 && width > 40 && height > 30 && size !== null ? (<>
+            <text
+              className={twMerge(vestPubkey.toBase58() !== PublicKey.default.toBase58() ? 'cursor-pointer' : '')}
+              x={x + width / 2}
+              y={y + height / 2 + (claimedAmountFormatted !== '0' ? 0 : 4)}
+              textAnchor="middle"
+              fill="#fff"
+              fontSize={width > 50 ? 10 : width > 40 ? 8 : 6}
+              onClick={() => {
+                window.open(getAccountExplorer(vestPubkey), '_blank');
+              }}
+            >
+              {num}
+            </text>
+
+            {claimedAmountFormatted !== '0' ? <text
+              className={twMerge('opacity-60', vestPubkey.toBase58() !== PublicKey.default.toBase58() ? 'cursor-pointer' : '')}
+              x={x + width / 2}
+              y={y + height / 2 + 12}
+              textAnchor="middle"
+              fill="#fff"
+              fontSize={width > 50 ? 8 : width > 40 ? 6 : 4}
+              onClick={() => {
+                window.open(getAccountExplorer(vestPubkey), '_blank');
+              }}
+            >
+              {claimedAmountFormatted} {width > 80 ? 'claimed' : ''}
+            </text> : null}
+          </>
+          ) : null
+        }
+
+        {
+          type ? (
+            <text x={x + 6} y={y + 16} fill="#fff" fontSize={12} fillOpacity={1}>
+              {name}
+            </text>
+          ) : null
+        }
+      </g >
+    );
+  };
+
+export default function AllVestingChart({
+  vests,
+}: {
+  vests: VestExtended[] | null;
+}) {
+  const [data, setData] = useState<{
+    name: string;
+    type: 'team' | 'investor' | 'foundation';
+    color: string;
+    children: {
+      name: string;
+      vestPubkey: PublicKey;
+      claimedAmount: number;
+      size: number;
+      color: string;
+    }[];
+  }[] | null>(null);
+
+  useEffect(() => {
+    if (!vests) {
+      setData(null);
+      return;
+    }
+
+    const sortedVests = vests.sort((a, b) => b.amount.toNumber() - a.amount.toNumber());
+
+    setData([
+      {
+        name: 'Team vest',
+        type: 'team',
+        color: `transparent`,
+        children: sortedVests.filter((vest) => vest.voteMultiplier === 40000).map((vest) => ({
+          name: `${vest.owner.toBase58()} vest`,
+          claimedAmount: nativeToUi(vest.claimedAmount, window.adrena.client.adxToken.decimals),
+          vestPubkey: vest.pubkey,
+          size: nativeToUi(vest.amount, window.adrena.client.adxToken.decimals),
+          color: colors.team,
+        })),
+      },
+      {
+        name: 'Investors vest',
+        type: 'investor',
+        color: `transparent`,
+        children: sortedVests.filter((vest) => vest.voteMultiplier === 10000 && vest.originBucket === 0).map((vest) => ({
+          name: `${vest.owner.toBase58()} vest`,
+          claimedAmount: nativeToUi(vest.claimedAmount, window.adrena.client.adxToken.decimals),
+          vestPubkey: vest.pubkey,
+          size: nativeToUi(vest.amount, window.adrena.client.adxToken.decimals),
+          color: colors.investors,
+        })),
+      },
+      {
+        name: 'Foundation vest',
+        type: 'foundation',
+        color: `transparent`,
+        children: sortedVests.filter((vest) => vest.originBucket === 1).map((vest) => ({
+          name: `${vest.owner.toBase58()} vest`,
+          claimedAmount: nativeToUi(vest.claimedAmount, window.adrena.client.adxToken.decimals),
+          vestPubkey: vest.pubkey,
+          size: nativeToUi(vest.amount, window.adrena.client.adxToken.decimals),
+          color: colors.foundation,
+        })),
+      },
+    ]);
+  }, [vests]);;
+
+  if (!data || !data.length) {
+    return (
+      <div className="h-full w-full flex items-center justify-center text-sm">
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className='flex flex-col w-full h-full items-center'>
+      <ResponsiveContainer width="100%" height="100%">
+        <Treemap
+          width={400}
+          height={400}
+          data={data}
+          dataKey="size"
+          isAnimationActive={false}
+          // Note: Needs to provide keys for typescript to be happy, even though Treemap is filling up the keys
+          content={<CustomizedContent root={undefined} depth={0} x={0} y={0} width={0} height={0} index={0} payload={undefined} claimedAmount={0} vestPubkey={PublicKey.default} color={''} rank={0} name={''} size={0} />}>
+        </Treemap>
+      </ResponsiveContainer>
+    </div >
+  );
+}
---
+++ src/components/pages/global/Emissions/EmissionsChart.tsx
@@ -0,0 +1,170 @@
+import { useMemo, useState } from 'react';
+
+import Loader from '@/components/Loader/Loader';
+import LineRechart from '@/components/ReCharts/LineRecharts';
+import { ADRENA_EVENTS } from '@/constant';
+import useVestRegistry from '@/hooks/useVestRegistry';
+
+interface EmissionsChartProps {
+  isSmallScreen: boolean;
+}
+
+const lmEmissionsStart = new Date(1727006400000);
+const startDate = new Date(1726591514000);
+
+type Emissions = {
+  date: Date;
+  time: string;
+  adxLiquidityMining: number;
+  alpLiquidityMining: number;
+  genesisLiquidityMining: number;
+  investorVesting: number;
+  launchTeamVesting: number;
+  foundationVesting: number;
+}[];
+
+// in percent
+const alpLmEmissionDecay = 10;
+const adxLmEmissionDecayYear1 = 7;
+const adxLmEmissionDecayYear2 = 12;
+
+const alpLmPerMonth = 15000000;
+const adxLmPerMonth = 8600000;
+
+// This unit is used in backend to calculate the emission - it's not pixel perfect, but it's close enough
+const secondsPerMonth = 2592000;
+
+// There are 4 rounds of 6 hours per day
+const alpLmPerDay = (alpLmPerMonth / secondsPerMonth) * 21600;
+const adxLmPerDay = (adxLmPerMonth / secondsPerMonth) * 21600;
+const startOfLmLpMonthCounter = new Date(1726591514000);
+
+const vestingStart = new Date(1734696000000);
+const vestingEnd = new Date(1789905600000);
+
+const investorVestPerDay = (156650000 / ((vestingEnd.getTime() - vestingStart.getTime()) / (1000 * 60 * 60 * 24))) * 50 / 100;
+const launchTeamVestPerDay = (202350000 / ((vestingEnd.getTime() - vestingStart.getTime()) / (1000 * 60 * 60 * 24))) * 50 / 100;
+const foundationVestPerDay = (90000000 / ((vestingEnd.getTime() - vestingStart.getTime()) / (1000 * 60 * 60 * 24))) * 50 / 100;
+
+const genesisStartDate = new Date(1726574400000);
+const genesisEndDate = new Date(1742385600000);
+
+const genesisAdxRewards = 50_000_000;
+const genesisEmissionPerDay = genesisAdxRewards / ((genesisEndDate.getTime() - genesisStartDate.getTime()) / (1000 * 60 * 60 * 24));
+
+export function EmissionsChart({ isSmallScreen }: EmissionsChartProps) {
+  const emissions: Emissions = useMemo(() => {
+    // Generate all timestamps
+    const emissions = Array.from({ length: 770 }).reduce((acc: Emissions, _, i) => {
+      const d = new Date(startDate.getTime());
+      d.setDate(d.getDate() + i);
+
+      acc.push({
+        date: d,
+        time: d.toLocaleDateString('en-US', {
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric',
+          timeZone: 'UTC',
+        }),
+        adxLiquidityMining: 0,
+        alpLiquidityMining: 0,
+        genesisLiquidityMining: 0,
+        investorVesting: 0,
+        launchTeamVesting: 0,
+        foundationVesting: 0,
+      })
+
+      return acc;
+    }, [] as Emissions);
+
+    // Calculate LM emissions
+
+    let monthsSinceStart = 0;
+    let lastMonthUpdate = startOfLmLpMonthCounter;
+    let alpLmPerDayDyn = alpLmPerDay;
+    let adxLmPerDayDyn = adxLmPerDay;
+
+    // Handle ADX/ALP staking LM emissions
+    emissions.forEach((emission) => {
+      if (emission.date.getTime() >= (lastMonthUpdate.getTime() + (secondsPerMonth * 1000))) {
+        // One month as passed, apply decay
+        monthsSinceStart += 1;
+        lastMonthUpdate = new Date(lastMonthUpdate.getTime() + (secondsPerMonth * 1000));
+
+        alpLmPerDayDyn = alpLmPerDayDyn - (alpLmPerDayDyn * alpLmEmissionDecay / 100);
+
+        const adxLmEmissionDecay = monthsSinceStart < 12 ? adxLmEmissionDecayYear1 : adxLmEmissionDecayYear2;
+
+        adxLmPerDayDyn = adxLmPerDayDyn - (adxLmPerDayDyn * adxLmEmissionDecay / 100);
+      }
+
+      if (emission.date.getTime() < lmEmissionsStart.getTime()) {
+        return;
+      }
+
+      emission.alpLiquidityMining = alpLmPerDayDyn;
+      emission.adxLiquidityMining = adxLmPerDayDyn;
+    });
+
+    // Handle vesting
+    emissions.forEach((emission) => {
+      if (emission.date.getTime() <= vestingStart.getTime() || emission.date.getTime() >= vestingEnd.getTime()) {
+        return;
+      }
+
+      emission.investorVesting = investorVestPerDay;
+      emission.launchTeamVesting = launchTeamVestPerDay;
+      emission.foundationVesting = foundationVestPerDay;
+    });
+
+    // Handle genesis
+    emissions.forEach((emission) => {
+      if (emission.date.getTime() <= genesisStartDate.getTime() || emission.date.getTime() >= genesisEndDate.getTime()) {
+        return;
+      }
+
+      emission.genesisLiquidityMining = genesisEmissionPerDay;
+    });
+
+    return emissions;
+  }, []);
+
+  return (
+    <LineRechart
+      title=""
+      formatY="number"
+      isNowReferenceLine={true}
+      data={emissions.map(({
+        time,
+        alpLiquidityMining,
+        adxLiquidityMining,
+        genesisLiquidityMining,
+        investorVesting,
+        launchTeamVesting,
+        foundationVesting,
+      }) => ({
+        time,
+        'ALP LM': alpLiquidityMining,
+        'ADX LM': adxLiquidityMining,
+        'ALP Genesis LM': genesisLiquidityMining,
+        'Investor Vesting': investorVesting,
+        'Launch Team Vesting': launchTeamVesting,
+        'Foundation Vesting': foundationVesting,
+      }))}
+      labels={[
+        { name: 'ALP LM', color: '#256281' },
+        { name: 'ADX LM', color: '#a82e2e' },
+        { name: 'ALP Genesis LM', color: '#7ed7c1' },
+        { name: 'Investor Vesting', color: '#9e8cae' },
+        { name: 'Launch Team Vesting', color: '#d4c7df' },
+        { name: 'Foundation Vesting', color: '#eb6672' },
+      ]}
+      scale="sqrt"
+      periods={[]}
+      yDomain={[0]}
+      gmt={0}
+      isSmallScreen={isSmallScreen}
+    />
+  );
+}
