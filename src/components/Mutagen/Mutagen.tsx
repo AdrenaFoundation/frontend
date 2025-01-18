c4ce0d19f942c42eebf09428b6f64beeec2c5b3d
Orex <adrena.orex@gmail.com>
2025-01-18 18:07:22 +0400

    save

--- src/components/Mutagen/Mutagen.tsx
+++ src/components/Mutagen/Mutagen.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-import mutagenIcon from '../../../public/images/Icons/fuel-pump-fill.svg';
+import mutagenIcon from '../../../public/images/mutagen-10.png';
 import Button from '../common/Button/Button';
 import Menu from '../common/Menu/Menu';
 import Modal from '../common/Modal/Modal';
@@ -57,15 +57,15 @@ export default function Mutagen({
     <Menu
       openMenuTriggerType='hover'
       trigger={
-        <div className='gap-x-2 flex items-center justify-center bg-[#1f2c3c] rounded-full pl-6 pr-6'>
-          <div>-</div>
+        <div className='gap-x-2 flex items-center justify-center bg-[#1f2c3c] rounded-full pl-3 pr-2 pt-1 pb-1 w-20'>
+          <div className='text-xs font-boldy'>1650</div>
 
           <Image
             src={mutagenIcon}
             alt={'mutagen'}
-            width='12'
-            height='12'
-            className={'w-3 h-3'}
+            width='40'
+            height='40'
+            className={'w-5 h-5'}
           />
         </div>
       }
