c204a0c4f8bf1874cba747c3737a8e2a3d92b3fc
Orex <adrena.orex@gmail.com>
2025-01-22 08:04:44 +0400

    Fix mobile

--- src/pages/ranked/Expanse/Expanse.tsx
+++ src/pages/ranked/Expanse/Expanse.tsx
@@ -24,7 +24,7 @@ export default function Expanse() {
 
                 <div>
                     <p className="font-boldy mb-3 mt-4 xl:mt-0 flex text-base justify-center xl:justify-start">Total Contest Rewards</p>
-                    <div className="flex flex-row gap-2 items-center bg-[#0D1923] px-8 py-3 rounded-lg border border-white/5 mb-3">
+                    <div className="flex flex-row gap-2 items-center justify-center md:justify-start bg-[#0D1923] px-8 py-3 rounded-lg border border-white/5 mb-3">
                         <Image
                             src={window.adrena.client.adxToken.image}
                             alt="ADX logo"
@@ -35,7 +35,7 @@ export default function Expanse() {
                         </p>
                     </div>
 
-                    <div className="flex flex-row gap-1 items-center bg-[#0D1923] px-8 py-3 rounded-lg border border-white/5">
+                    <div className="flex flex-row gap-1 items-center justify-center md:justify-start bg-[#0D1923] px-8 py-3 rounded-lg border border-white/5">
                         <Image src={jtoLogo} alt="JTO logo" className="w-5 h-5" />
                         <p className="text-xl font-mono">
                             50k JTO <span>Rewards</span>
