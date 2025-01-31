d6dd426853b4335ddd09d51c572f401762426e07
Orex <adrena.orex@gmail.com>
2025-01-31 19:14:12 +0400

    nit

--- src/components/QuestMenu/StreakComp.tsx
+++ src/components/QuestMenu/StreakComp.tsx
@@ -41,7 +41,6 @@ export default function StreakComp({
                 </div>
             </div>
 
-
             <div className='flex w-full justify-between items-center'>
                 <div>
                     <div className='flex gap-1'>
@@ -95,102 +94,6 @@ export default function StreakComp({
                     </div>
                 </div>
             </div>
-
-            {/* 
-
-            <div className='flex w-full items-center justify-evenly'>
-                <div className='flex gap-1'>
-                    <div className='flex gap-1'>
-                        <div className="text-sm font-boldy">0</div>
-                        <div className="text-sm font-boldy">
-                            Daily
-                        </div>
-                    </div>
-
-                    <div className='font-boldy text-xs text-[#e47dbb]'>
-                        +0.7 mutagen
-                    </div>
-                </div>
-
-                <div className='flex gap-1'>
-                    <div className='flex gap-1'>
-                        <div className="text-sm font-boldy">0</div>
-                        <div className="text-sm font-boldy">
-                            Weekly
-                        </div>
-                    </div>
-
-                    <div className='font-boldy text-xs text-[#e47dbb]'>
-                        +1 mutagen
-                    </div>
-                </div>
-
-                <div className='flex gap-1'>
-                    <div className='flex gap-1'>
-                        <div className="text-sm font-boldy">0</div>
-                        <div className="text-sm font-boldy">
-                            Monthly
-                        </div>
-                    </div>
-
-                    <div className='font-boldy text-xs text-[#e47dbb]'>
-                        +2 mutagen
-                    </div>
-                </div>
-            </div> */}
-
-            {/* <div className="flex justify-between items-center">
-                <div className="flex flex-col items-center">
-                    <span className="text-white/50 text-sm mb-2">Trade Daily</span>
-                    <div className="flex items-center gap-1">
-                        {streak.pointsDays > 0 ? (
-                            <>
-                                <span className="font-mono text-sm animate-text-shimmer bg-clip-text text-transparent bg-[length:300%_100%] bg-[linear-gradient(110deg,#8DC52E,45%,#FFFA5D,60%,#8DC52E)]">+{streak.pointsDays}</span>
-                                <Image src={needle} alt="needle" className="w-[20px] h-[20px] inline-block" />
-                            </>
-                        ) : (
-                            <>
-                                <span className="font-mono text-sm animate-text-shimmer bg-clip-text text-transparent bg-[length:300%_100%] bg-[linear-gradient(110deg,#FA6724,45%,#FAD524,55%,#FA6724)]">0/0.25</span>
-                                <Image src={needle} alt="needle" className="w-[20px] h-[20px] inline-block" />
-                            </>
-                        )}
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center">
-                    <span className="text-white/50 text-sm mb-2">Trade for 7 consecutive days</span>
-                    <div className="flex items-center gap-1">
-                        {streak.pointsWeeks > 0 ? (
-                            <>
-                                <span className="font-mono text-sm animate-text-shimmer bg-clip-text text-transparent bg-[length:300%_100%] bg-[linear-gradient(110deg,#8DC52E,45%,#FFFA5D,60%,#8DC52E)]">+{streak.pointsWeeks}</span>
-                                <Image src={needle} alt="needle" className="w-[20px] h-[20px] inline-block" />
-                            </>
-                        ) : (
-                            <>
-                                <span className="font-mono text-sm animate-text-shimmer bg-clip-text text-transparent bg-[length:300%_100%] bg-[linear-gradient(110deg,#FA6724,45%,#FAD524,55%,#FA6724)]">0/1</span>
-                                <Image src={needle} alt="needle" className="w-[20px] h-[20px] inline-block" />
-                            </>
-                        )}
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center">
-                    <span className="text-white/50 text-sm mb-2">Trade for 30 consecutive days</span>
-                    <div className="flex items-center gap-1">
-                        {streak.pointsMonths > 0 ? (
-                            <>
-                                <span className="font-mono text-sm animate-text-shimmer bg-clip-text text-transparent bg-[length:300%_100%] bg-[linear-gradient(110deg,#8DC52E,45%,#FFFA5D,60%,#8DC52E)]">+{streak.pointsMonths}</span>
-                                <Image src={needle} alt="needle" className="w-[20px] h-[20px] inline-block" />
-                            </>
-                        ) : (
-                            <>
-                                <span className="font-mono text-sm animate-text-shimmer bg-clip-text text-transparent bg-[length:300%_100%] bg-[linear-gradient(110deg,#FA6724,45%,#FAD524,55%,#FA6724)]">0/2</span>
-                                <Image src={needle} alt="needle" className="w-[20px] h-[20px] inline-block" />
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 }
