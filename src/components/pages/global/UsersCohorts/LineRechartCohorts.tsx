c90ce7bde8840dc852b6b79e1552d2a1baaea5c4
Orex <122088118+adrena-orex@users.noreply.github.com>
2025-02-03 17:11:14 +0400

    Cohorts retention (#788)

--- src/components/pages/global/UsersCohorts/LineRechartCohorts.tsx
+++ src/components/pages/global/UsersCohorts/LineRechartCohorts.tsx
@@ -6,15 +6,118 @@ import {
   LineChart,
   ResponsiveContainer,
   Tooltip,
+  TooltipProps,
   XAxis,
   YAxis,
 } from 'recharts';
+import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import { DataKey } from 'recharts/types/util/types';
+import { twMerge } from 'tailwind-merge';
 
 import { RechartsData } from '@/types';
-import { formatNumberShort, formatToWeekOf } from '@/utils';
+import { formatNumber, formatNumberShort, formatPercentage, formatPriceInfo, formatToWeekOf } from '@/utils';
 
-import CustomRechartsToolTip from '../../../CustomRechartsToolTip/CustomRechartsToolTip';
+function CustomRechartsToolTip({
+  data,
+  active,
+  payload,
+  label,
+  precision = 2,
+  format = 'currency',
+  labelCustomization,
+  totalColor = 'red',
+}: TooltipProps<ValueType, NameType> & {
+  data: RechartsData[];
+  precision?: number;
+  format?: 'currency' | 'percentage' | 'number';
+  labelCustomization?: (label: string) => string;
+  totalColor?: string;
+}) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="bg-third p-3 border border-white rounded-lg min-w-[12em]">
+        {label && <p className="text-lg mb-2 font-mono">{labelCustomization ? labelCustomization(label) : label}</p>}
+
+        <table className='w-full'>
+          <thead>
+            <th className='text-xs font-boldy'>Name</th>
+            <th className='text-xs font-boldy'>Date</th>
+            <th className='text-xs font-boldy'>Traders</th>
+            <th className='text-xs font-boldy'>Retention</th>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className='text-center'>
+                <span
+                  style={{ color: totalColor }}
+                >
+                  Total
+                </span>
+              </td>
+
+              <td className='text-center'>
+                -
+              </td>
+
+              <td className='text-center text-sm font-mono' style={{ color: totalColor }}>
+                {(() => {
+                  const v = payload.reduce((acc, item) => acc + Number(item.value), 0);
+
+                  return format === 'currency'
+                    ? formatPriceInfo(Number(v), precision, precision)
+                    : format === 'percentage'
+                      ? formatPercentage(Number(v), precision)
+                      : formatNumber(Number(v), precision)
+                })()}
+              </td>
+
+              <td className='text-center text-txtfade'>
+                -
+              </td>
+            </tr>
+
+            {payload.map((item) => {
+              const originalData = data.find(x => x && item.dataKey && !!x[item.dataKey]);
+
+              const originalNumber = originalData && item.dataKey ? Number(originalData[item.dataKey]) : 0;
+              const originalPercentage = Number(item.value) * 100 / originalNumber;
+
+              return <tr key={item.dataKey}>
+                <td className='text-center'>
+                  <span style={{ color: item.color }}>{item.dataKey}</span>
+                </td>
+
+                <td className='text-center'>
+                  <span style={{ color: item.color }}>{originalData?.['time']}</span>
+                </td>
+
+                <td className='text-center'>
+                  <span
+                    className={twMerge('font-mono')}
+                    style={{ color: item.color }}
+                  >
+                    {format === 'currency'
+                      ? formatPriceInfo(Number(item.value), precision, precision)
+                      : format === 'percentage'
+                        ? formatPercentage(Number(item.value), precision)
+                        : formatNumber(Number(item.value), precision)}
+                  </span>
+                </td>
+
+                <td className='text-center text-sm font-mono text-txtfade'>
+                  {formatPercentage(originalPercentage, precision)}
+                </td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div >
+    );
+  }
+
+  return null;
+}
 
 export default function LineRechartCohorts({
   data,
@@ -46,14 +149,14 @@ export default function LineRechartCohorts({
         <YAxis domain={['auto', 'auto']} tickFormatter={formatYAxis} fontSize="11" scale={"log"} />
 
         <Tooltip
+          wrapperStyle={{ zIndex: 20 }}
           content={
             <CustomRechartsToolTip
+              data={data}
               labelCustomization={(label: string) => {
                 return `from ${formatToWeekOf(label)} to ${formatToWeekOf(label, 1)}`;
               }}
-              isValueOnly={labels.length === 1}
               format={format}
-              total={true}
             />
           }
           cursor={false}
@@ -71,6 +174,7 @@ export default function LineRechartCohorts({
                   (l) => l !== String(e.dataKey).trim(),
                 ) as DataKey<string | number>[];
               }
+
               return [
                 ...hiddenLabels,
                 String(e.dataKey).trim() as DataKey<string | number>,
