525e75c01829180ece80f160369ea91fa02726c7
adrena-kino <adrena.kino@gmail.com>
2025-02-19 14:08:08 +0400

    make error message responsive

--- src/components/pages/trading/TradingInputs/LongShortTradingInputs/ErrorDisplay.tsx
+++ src/components/pages/trading/TradingInputs/LongShortTradingInputs/ErrorDisplay.tsx
@@ -13,20 +13,22 @@ export const ErrorDisplay = ({ errorMessage }: ErrorDisplayProps) => {
     return (
         <AnimatePresence>
             <motion.div
-                className="flex w-full h-auto relative overflow-hidden pl-6 pt-2 pb-3 pr-2 mt-1 sm:mt-2 border-2 border-[#BE3131] backdrop-blur-md z-30 items-center justify-center rounded-xl"
+                className="flex w-full relative overflow-hidden mt-1 sm:mt-2 border-2 border-[#BE3131] backdrop-blur-md z-30 rounded-xl"
                 initial={{ opacity: 0, scaleY: 0 }}
                 animate={{ opacity: 1, scaleY: 1 }}
                 exit={{ opacity: 0, scaleY: 0 }}
                 transition={{ duration: 0.5 }}
                 style={{ originY: 0 }}
             >
-                <Image
-                    className="w-auto h-[1.5em] absolute left-[0.5em]"
-                    src={errorImg}
-                    alt="Error icon"
-                />
-                <div className="items-center justify-center">
-                    <div className="text-sm">{errorMessage}</div>
+                <div className="flex items-center w-full py-2 px-4">
+                    <Image
+                        className="w-auto h-[1.5em] mr-3 flex-shrink-0"
+                        src={errorImg}
+                        alt="Error icon"
+                    />
+                    <div className="flex-1 text-sm 2xl:text-center">
+                        {errorMessage}
+                    </div>
                 </div>
             </motion.div>
         </AnimatePresence>
