c24adaede8951fdd939d17757f4aeafceb78089b
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2025-02-21 17:41:00 +0400

    nit

--- src/config/openai.ts
+++ src/config/openai.ts
@@ -1,6 +1,5 @@
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
-  apiKey:
-    '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',
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
