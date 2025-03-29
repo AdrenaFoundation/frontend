4effefee8f76dc213b48dedb821dac7d62b67063
Orex <adrena.orex@gmail.com>
2025-03-29 17:31:56 +0400

    Add Achievement sort

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -4,27 +4,23 @@ export default function Checkbox({
   checked,
   onChange,
   className,
+  variant = 'default',
 }: {
   checked?: boolean;
   onChange: (checked: boolean) => void;
   className?: string;
+  variant?: 'default' | 'white';
 }) {
   return (
     <div
       className={twMerge(
-        'h-3 w-3 border border-bcolor rounded-[0.1em] cursor-pointer flex justify-center items-center',
+        'h-3 w-3 border border-bcolor rounded-[0.2em] cursor-pointer flex justify-center items-center',
+        checked
+          ? "bg-[url('/images/check.svg')] bg-no-repeat bg-center bg-contain"
+          : variant === 'white' ? 'bg-white' : '',
         className,
       )}
       onClick={() => onChange(!checked)}
-    >
-      {checked ? (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img
-          src="images/check.svg"
-          alt="check icon"
-          className="h-full w-full"
-        />
-      ) : null}
-    </div>
+    />
   );
 }
