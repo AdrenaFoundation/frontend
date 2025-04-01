51076164abdc985d4123a1619533832f65be2686
Orex <adrena.orex@gmail.com>
2025-04-01 15:44:10 +0400

    Handle address lookup table

---
+++ src/hooks/useAllAddressLookupTables.tsx
@@ -0,0 +1,40 @@
+import { AddressLookupTableState, PublicKey } from '@solana/web3.js';
+import { useEffect, useState } from 'react';
+
+export function useAllAddressLookupTables({
+    wallet,
+}: {
+    wallet: PublicKey | null;
+}): {
+    allAddressLookupTables: { pubkey: PublicKey; account: AddressLookupTableState }[] | null;
+    triggerAllAddressLookupTablesReload: () => void;
+} {
+    const [trickReload, triggerReload] = useState<number>(0);
+    const [allAddressLookupTables, setAllAddressLookupTables] = useState<{ pubkey: PublicKey; account: AddressLookupTableState }[] | null>(null);
+
+    useEffect(() => {
+        const loadAllAddressLookupTables = async () => {
+            if (!wallet) return setAllAddressLookupTables(null);
+
+            try {
+                setAllAddressLookupTables(await window.adrena.client.getAllLookupTablesByAuthority(wallet));
+            } catch (e) {
+                console.log('Error loading address lookup tables', e);
+            }
+        };
+
+        loadAllAddressLookupTables();
+
+        const interval = setInterval(loadAllAddressLookupTables, 60000);
+
+        return () => clearInterval(interval);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [trickReload, window.adrena.client.readonlyConnection]);
+
+    return {
+        allAddressLookupTables,
+        triggerAllAddressLookupTablesReload: () => {
+            triggerReload(trickReload + 1);
+        },
+    };
+}
