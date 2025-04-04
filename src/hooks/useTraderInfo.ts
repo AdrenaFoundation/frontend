b13c1e86f0d51a7a55fb949fa0b87cc8cdfc4621
Orex <adrena.orex@gmail.com>
2025-04-04 17:52:15 +0400

    save

--- src/hooks/useTraderInfo.ts
+++ src/hooks/useTraderInfo.ts
@@ -1,7 +1,10 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-import DataApiClient from '@/DataApiClient';
-import { EnrichedTraderInfo } from '@/types';
+import DataApiClient from "@/DataApiClient";
+import { EnrichedTraderInfo } from "@/types";
+
+import useAwakeningV2 from "./useAwakeningV2";
+import useExpanseData from "./useExpanseData";
 
 export default function useTraderInfo({
   walletAddress,
@@ -10,9 +13,63 @@ export default function useTraderInfo({
 }): {
   traderInfo: EnrichedTraderInfo | null;
   triggerTraderInfoReload: () => void;
+  expanseRanking: {
+    rank: number;
+    championshipPoints: number;
+    volume: number;
+    pnl: number;
+  } | null;
+  awakeningRanking: {
+    division: string;
+    pnl: number;
+    rank: number;
+    volume: number;
+  } | null;
 } {
   const [trickReload, triggerReload] = useState<number>(0);
   const [traderInfo, setTraderInfo] = useState<EnrichedTraderInfo | null>(null);
+
+  const expanseLeaderboard = useExpanseData({ allUserProfilesMetadata: [] });
+  const awakeningLeaderboard = useAwakeningV2({
+    wallet: walletAddress
+      ? {
+          adapterName: "Phantom",
+          walletAddress,
+        }
+      : null,
+    allUserProfilesMetadata: [],
+  });
+
+  const expanseRanking = useMemo(() => {
+    if (!walletAddress) return null;
+
+    const d = expanseLeaderboard?.seasonLeaderboard.find(
+      (rank) => rank.wallet.toBase58() === walletAddress,
+    );
+
+    if (!d) return null;
+
+    return {
+      rank: d.rank,
+      championshipPoints: d.championshipPoints,
+      volume: d.volume,
+      pnl: d.pnl,
+    };
+  }, [expanseLeaderboard?.seasonLeaderboard, walletAddress]);
+
+  const awakeningRanking = useMemo(() => {
+    if (!walletAddress) return null;
+
+    if (!awakeningLeaderboard || !awakeningLeaderboard?.currentUserData)
+      return null;
+
+    return {
+      division: awakeningLeaderboard.currentUserData.division,
+      pnl: awakeningLeaderboard.currentUserData.pnl,
+      rank: awakeningLeaderboard.currentUserData.rank,
+      volume: awakeningLeaderboard.currentUserData.volume,
+    };
+  }, [awakeningLeaderboard?.currentUserData, walletAddress]);
 
   const loadTraderInfo = useCallback(
     async () => {
@@ -37,7 +94,7 @@ export default function useTraderInfo({
       try {
         setTraderInfo(await fetchTraderInfo());
       } catch (e) {
-        console.log('Error loading trader info', e, String(e));
+        console.log("Error loading trader info", e, String(e));
         throw e;
       }
     },
@@ -63,5 +120,7 @@ export default function useTraderInfo({
     triggerTraderInfoReload: () => {
       triggerReload(trickReload + 1);
     },
+    expanseRanking,
+    awakeningRanking,
   };
 }
