8b1a90e94348f4fea9b5713ef0dba36681c2cf68
adrena-kino <adrena.kino@gmail.com>
2025-04-09 15:13:37 +0400

    Fix select text better way

--- src/components/common/Select/Select.tsx
+++ src/components/common/Select/Select.tsx
@@ -117,7 +117,7 @@ export default function Select<T extends string>({
 
                   <MenuItem
                     disabled={option.disabled}
-                    className={twMerge("flex flex-row items-center justify-end relative overflow-hidden h-14", menuItemClassName)}
+                    className={twMerge("flex flex-row items-center relative overflow-hidden h-14", menuItemClassName, option?.img ? "justify-end" : "justify-center")}
                     onMouseEnter={() => setOptionHover(i)}
                     onMouseLeave={() => setOptionHover(null)}
                     onClick={() => {
@@ -140,7 +140,7 @@ export default function Select<T extends string>({
                       />
                     ) : null}
 
-                    <span className={twMerge("font-boldy text-lg z-20 ", menuTextClassName, option?.img ? "" : "m-auto")}>
+                    <span className={twMerge("font-boldy text-lg z-20", menuTextClassName)}>
                       {option.title}
                     </span>
                   </MenuItem>
