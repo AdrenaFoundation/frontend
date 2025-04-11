c622a10f6ad3993039aba94df810064a35b47d64
Orex <adrena.orex@gmail.com>
2025-04-11 16:32:12 +0400

    save

--- src/pages/ranked/Factions.tsx
+++ src/pages/ranked/Factions.tsx
@@ -1,9 +1,13 @@
+import Modal from '@/components/common/Modal/Modal';
+import ViewProfileModal from '@/components/pages/profile/ViewProfileModal';
 import { useAllUserProfilesMetadata } from '@/hooks/useAllUserProfilesMetadata';
 import useBetterMediaQuery from '@/hooks/useBetterMediaQuery';
 import useInterseason2Data from '@/hooks/useInterseason2Data';
-import { SeasonLeaderboardsData } from '@/types';
-import { getAbbrevWalletAddress } from '@/utils';
-import React, { useMemo } from 'react';
+import { SeasonLeaderboardsData, UserProfileExtended } from '@/types';
+import { formatNumber, getAbbrevWalletAddress } from '@/utils';
+import { PublicKey } from '@solana/web3.js';
+import { AnimatePresence } from 'framer-motion';
+import React, { useMemo, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 const teamAColor = "#A2E3FA";
@@ -13,35 +17,54 @@ function Rank({
     team,
     rank,
     user,
+    className,
+    setActiveProfile,
 }: {
     team: 'A' | 'B';
     rank: 'Sergeant' | 'Lieutenant' | 'General';
     user?: SeasonLeaderboardsData['seasonLeaderboard'][number];
+    className?: string;
+    setActiveProfile: (u: UserProfileExtended | null) => void;
 }) {
     return (
         <div className={twMerge(
-            'relative aspect-square bg-cover bg-no-repeat bg-center border-2 border-transparent bg-origin-border bg-clip-border rounded',
-            `shadow-[0_0_10px_#5AA6FA,0_0_20px_${team === 'A' ? teamAColor : teamBColor}]`,
+            'relative',
             ({
-                General: 'w-[15em]',
-                Lieutenant: 'w-[10em]',
-                Sergeant: 'w-[6em]',
+                General: 'w-[19em] h-[25em] border-2',
+                Lieutenant: 'w-[14em] h-[23em] border-2',
+                Sergeant: 'w-[10em] h-[20em] border-2',
             } as const)[rank],
-        )}>
+            // `shadow-[0_0_10px_#5AA6FA,0_0_20px_${team === 'A' ? teamAColor : teamBColor}]`,
+        )}
+            style={{
+                // borderColor: rank === 'General' ? team === 'A' ? teamAColor : teamBColor : '',
+            }}
+        >
             <div
-                className='absolute bg-cover bg-no-repeat bg-center bg-origin-border bg-clip-border w-full h-full z-10 opacity-50'
+                className={twMerge('absolute bg-cover bg-no-repeat bg-center w-full h-full z-10 opacity-50', className)}
                 style={{
                     backgroundImage: `url(images/${team}-${rank.toLowerCase()}.png)`,
                 }}
             />
 
+            <div className={twMerge(
+                'absolute flex flex-col w-full left-0 items-center justify-center font-archivo tracking-[0.3em] uppercase text-txtfade',
+                ({
+                    General: 'text-[0.9em] -top-10',
+                    Lieutenant: 'text-[0.7em] -top-8',
+                    Sergeant: 'text-[0.5em] -top-6',
+                } as const)[rank],
+            )}>
+                {rank}
+            </div>
+
             <div
                 className={twMerge(
-                    'z-20 bg-contain bg-no-repeat bg-center rounded-full top-2 right-2 absolute',
+                    'z-20 bg-contain bg-no-repeat bg-center rounded-full top-2 right-2 absolute grayscale',
                     ({
-                        General: 'w-[1.5em] h-[1.5em]',
-                        Lieutenant: 'w-[1.3em] h-[1.3em]',
-                        Sergeant: 'w-[0.6em] h-[0.6em]',
+                        General: 'w-[2em] h-[2em]',
+                        Lieutenant: 'w-[1.5em] h-[1.5em]',
+                        Sergeant: 'w-[1em] h-[1em]',
                     } as const)[rank],
                 )}
                 style={{
@@ -50,16 +73,42 @@ function Rank({
             />
 
             <div className={twMerge(
-                'absolute -bottom-12 flex flex-col w-full left-0 items-center justify-center',
+                'absolute -bottom-10 flex flex-col w-full left-0 items-center justify-center',
             )}>
-                <div className='font-archivo tracking-widest text-xs'>
-                    {rank}
-                </div>
+                {user ? <div
+                    className={twMerge('font-archivo tracking-widest text-xs cursor-pointer hover:underline')}
+                    style={{
+                        color: team === 'A' ? teamAColor : teamBColor,
+                    }}
+                    onClick={async () => {
+                        const p = await window.adrena.client.loadUserProfile({ user: user.wallet });
 
-                {user ? <div className={twMerge('font-archivo tracking-widest text-xs')} style={{
-                    color: team === 'A' ? teamAColor : teamBColor,
-                }}>
+                        if (p === false) {
+                            setActiveProfile({
+                                version: -1, // Not a real profile
+                                pubkey: PublicKey.default, // Not a real profile
+                                nickname: getAbbrevWalletAddress(user.wallet.toBase58()),
+                                createdAt: Date.now(),
+                                owner: user.wallet,
+                                referrerProfile: null,
+                                claimableReferralFeeUsd: 0,
+                                totalReferralFeeUsd: 0,
+                                profilePicture: 0,
+                                wallpaper: 0,
+                                title: 0,
+                                achievements: [],
+                            });
+                        } else {
+                            setActiveProfile(p);
+                        }
+                    }}
+                >
                     {user.nickname && user.nickname.length ? user.nickname : getAbbrevWalletAddress(user.wallet.toBase58())}
+                </div> : null}
+
+                {user ? <div className={twMerge('font-archivo tracking-widest text-xs text-[#ff47b5]')} style={{
+                }}>
+                    {formatNumber(user.totalPoints, 2)} mutagens
                 </div> : null}
             </div>
         </div >
@@ -67,6 +116,8 @@ function Rank({
 }
 
 export default function Factions() {
+    const [activeProfile, setActiveProfile] =
+        useState<UserProfileExtended | null>(null);
     const isMobile = useBetterMediaQuery('(max-width: 1000px)');
 
     const { allUserProfilesMetadata } = useAllUserProfilesMetadata();
@@ -77,48 +128,59 @@ export default function Factions() {
     }, [data]);
 
     return (
-        <div className="w-full mx-auto relative flex flex-col pb-20 items-center gap-10">
-            <div className='text-sm sm:text-md tracking-[0.2rem] uppercase text-center'>Those who rise now will lead the next war</div>
+        <>
+            <div className="w-full mx-auto relative flex flex-col pb-20 items-center gap-10">
+                <div className='text-sm sm:text-md tracking-[0.2rem] uppercase text-center'>Those who rise now will lead the next war</div>
 
-            <div className="flex items-center gap-6 pl-4 pr-4 max-w-full">
+                <div className="flex items-center pl-4 pr-4 max-w-full mt-8">
 
-                {isMobile ? <div className='flex flex-wrap items-center justify-center gap-10'>
-                    {/* Team A */}
-                    <div className='flex flex-col items-center gap-20'>
-                        <Rank team='A' rank="General" user={top10?.[0]} />
-                        <Rank team='A' rank="Lieutenant" user={top10?.[2]} />
-                        <Rank team='A' rank="Sergeant" user={top10?.[4]} />
-                        <Rank team='A' rank="Sergeant" user={top10?.[6]} />
-                        <Rank team='A' rank="Sergeant" user={top10?.[8]} />
-                    </div>
+                    {isMobile ? <div className='flex flex-wrap items-center justify-center gap-10'>
+                        {/* Team A */}
+                        <div className='flex flex-col items-center gap-[6em] mt-16'>
+                            <Rank team='A' rank="General" user={top10?.[0]} setActiveProfile={setActiveProfile} />
+                            <Rank team='A' rank="Lieutenant" user={top10?.[2]} setActiveProfile={setActiveProfile} />
+                            <Rank team='A' rank="Sergeant" user={top10?.[4]} setActiveProfile={setActiveProfile} />
+                        </div>
 
-                    {/* Team B */}
-                    <div className='flex flex-col items-center gap-20'>
-                        <Rank team='B' rank="General" user={top10?.[1]} />
-                        <Rank team='B' rank="Lieutenant" user={top10?.[3]} />
-                        <Rank team='B' rank="Sergeant" user={top10?.[5]} />
-                        <Rank team='B' rank="Sergeant" user={top10?.[7]} />
-                        <Rank team='B' rank="Sergeant" user={top10?.[9]} />
-                    </div>
-                </div> : <>
-                    {/* Team A */}
+                        {/* Team B */}
+                        <div className='flex flex-col items-center gap-[6em] mt-16'>
+                            <Rank team='B' rank="General" user={top10?.[1]} setActiveProfile={setActiveProfile} />
+                            <Rank team='B' rank="Lieutenant" user={top10?.[3]} setActiveProfile={setActiveProfile} />
+                            <Rank team='B' rank="Sergeant" user={top10?.[5]} setActiveProfile={setActiveProfile} />
+                        </div>
+                    </div> : <div className='flex w-full items-end mt-8 gap-2'>
+                        {/* Team A */}
 
-                    <Rank team='A' rank="Sergeant" user={top10?.[4]} />
-                    <Rank team='A' rank="Sergeant" user={top10?.[6]} />
-                    <Rank team='A' rank="Sergeant" user={top10?.[8]} />
-                    <Rank team='A' rank="Lieutenant" user={top10?.[2]} />
-                    <Rank team='A' rank="General" user={top10?.[0]} />
+                        <Rank team='A' rank="Sergeant" user={top10?.[4]} setActiveProfile={setActiveProfile} />
+                        <Rank team='A' rank="Lieutenant" user={top10?.[2]} setActiveProfile={setActiveProfile} />
+                        <Rank team='A' rank="General" user={top10?.[0]} setActiveProfile={setActiveProfile} />
 
-                    {/* Team B */}
+                        {/* Team B */}
 
-                    <Rank team='B' rank="General" user={top10?.[1]} />
-                    <Rank team='B' rank="Lieutenant" user={top10?.[3]} />
-                    <Rank team='B' rank="Sergeant" user={top10?.[5]} />
-                    <Rank team='B' rank="Sergeant" user={top10?.[7]} />
-                    <Rank team='B' rank="Sergeant" user={top10?.[9]} />
-                </>}
+                        <Rank team='B' rank="General" user={top10?.[1]} setActiveProfile={setActiveProfile} />
+                        <Rank team='B' rank="Lieutenant" user={top10?.[3]} setActiveProfile={setActiveProfile} />
+                        <Rank team='B' rank="Sergeant" user={top10?.[5]} setActiveProfile={setActiveProfile} />
+                    </div>}
 
+                </div>
             </div>
-        </div>
+
+            <AnimatePresence>
+                {activeProfile ? (
+                    <Modal
+                        className="h-[80vh] w-full overflow-y-auto"
+                        wrapperClassName="items-start w-full max-w-[55em] sm:mt-0  bg-cover bg-center bg-no-repeat bg-[url('/images/wallpaper-1.jpg')]"
+                        title=""
+                        close={() => setActiveProfile(null)}
+                        isWrapped={false}
+                    >
+                        <ViewProfileModal
+                            profile={activeProfile}
+                            close={() => setActiveProfile(null)}
+                        />
+                    </Modal>
+                ) : null}
+            </AnimatePresence>
+        </>
     );
 }
