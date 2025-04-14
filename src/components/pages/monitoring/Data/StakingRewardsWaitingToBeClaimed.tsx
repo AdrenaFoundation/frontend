b1e5fb1c1a956a35e24e609e57ca12f01397f5d6
Orex <adrena.orex@gmail.com>
2025-04-14 16:27:05 +0400

    Remove unused info about ALP staking

--- src/components/pages/monitoring/Data/StakingRewardsWaitingToBeClaimed.tsx
+++ src/components/pages/monitoring/Data/StakingRewardsWaitingToBeClaimed.tsx
@@ -62,51 +62,27 @@ export default function StakingRewardsWaitingToBeClaimed({
     <div className="bg-[#050D14] border rounded-lg flex-1 shadow-xl">
       <div className="w-full border-b p-3">
         <p className={titleClassName}>
-          Staking Rewards (available, pending claims)
+          ADX Staking Rewards (available, pending claims)
         </p>
         <p className="text-xs opacity-50">
           Rewards from past rounds (resolved), waiting to be claimed.
         </p>
       </div>
 
-      <div className="flex flex-col sm:flex-row grow">
-        <div className="flex pl-5 pt-3 pb-1 pr-3 flex-col grow">
-          <div className='mb-2 text-xs sm:text-sm text-txtfade font-boldy uppercase'>ALP Staking</div>
+      <div className="flex pl-5 pt-3 pb-1 pr-3 flex-col grow border-t sm:border-l">
+        <NumberDisplay
+          nb={adxStakingPendingRewards}
+          precision={0}
+          suffix='USDC'
+          className='border-0 p-0 items-start'
+        />
 
-          <NumberDisplay
-            nb={alpStakingPendingRewards}
-            precision={0}
-            suffix='USDC'
-            className='border-0 p-0 items-start'
-          />
-
-          <NumberDisplay
-            nb={alpStakingPendingLmRewards}
-            precision={0}
-            suffix='ADX'
-            className='border-0 p-0 items-start'
-          />
-        </div>
-
-
-
-        <div className="flex pl-5 pt-3 pb-1 pr-3 flex-col grow border-t sm:border-l">
-          <div className='mb-2 text-xs sm:text-sm text-txtfade font-boldy uppercase'>ADX Staking</div>
-
-          <NumberDisplay
-            nb={adxStakingPendingRewards}
-            precision={0}
-            suffix='USDC'
-            className='border-0 p-0 items-start'
-          />
-
-          <NumberDisplay
-            nb={adxStakingPendingLmRewards}
-            precision={0}
-            suffix='ADX'
-            className='border-0 p-0 items-start'
-          />
-        </div>
+        <NumberDisplay
+          nb={adxStakingPendingLmRewards}
+          precision={0}
+          suffix='ADX'
+          className='border-0 p-0 items-start'
+        />
       </div>
     </div>
   );
