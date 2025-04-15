6cb72e1159690c15a3ca21718f591980b73e5af6
Orex <122088118+adrena-orex@users.noreply.github.com>
2025-04-15 09:20:10 +0400

    Add achievements in wallet digger (#944)

--- src/pages/monitoring/walletDigger.tsx
+++ src/pages/monitoring/walletDigger.tsx
@@ -22,19 +22,21 @@ import useTraderInfo from '@/hooks/useTraderInfo';
 import useUserProfile from '@/hooks/useUserProfile';
 import useUserVest from '@/hooks/useUserVest';
 import useWalletStakingAccounts from '@/hooks/useWalletStakingAccounts';
-import { ClaimHistoryExtended, LockedStakeExtended } from '@/types';
+import { ClaimHistoryExtended, LockedStakeExtended, PageProps } from '@/types';
 import { getAdxLockedStakes, getAlpLockedStakes, nativeToUi } from '@/utils';
 
 import chevronDown from '../../../public/images/chevron-down.svg';
 import shovelMonster from '../../../public/images/shovel-monster.png';
+import Achievements from '../achievements';
 
 const claimHistoryItemsPerPage = 4;
 
 export default function WalletDigger({
-    view
+    view,
+    ...props
 }: {
     view: string;
-}) {
+} & PageProps) {
     const [moreStakingInfo, setMoreStakingInfo] = useState(false);
     const [morePositionInfo, setMorePositionInfo] = useState(false);
 
@@ -424,6 +426,8 @@ export default function WalletDigger({
                 <VestStats vest={userVest} readonly={true} />
             </StyledContainer> : null
         }
+
+        {targetWalletPubkey && userProfile ? <Achievements {...props} userProfile={userProfile} defaultSort='points' defaultShowOwned={true} defaultShowNotOwned={false} /> : null}
 
         {
             targetWalletPubkey && expanseRanking && awakeningRanking ? <StyledContainer className="p-2 w-full" bodyClassName='gap-1'>
