ec06a3ef04c476875b485d0fdf89388d2c8a8cc0
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2025-05-30 12:23:08 +0400

    nit

--- src/components/pages/trading/ViewTabs/ViewTabs.tsx
+++ src/components/pages/trading/ViewTabs/ViewTabs.tsx
@@ -3,7 +3,6 @@ import { twMerge } from 'tailwind-merge';
 import crossIcon from '@/../public/images/Icons/cross.svg';
 import Button from '@/components/common/Button/Button';
 import FormatNumber from '@/components/Number/FormatNumber';
-
 import { PositionExtended } from '@/types';
 
 import { POSITION_BLOCK_STYLES } from '../Positions/PositionBlockComponents/PositionBlockStyles';
@@ -146,71 +145,6 @@ export default function ViewTabs({
                         />
                     ) : null}
                 </div>
-          
-        <div className="flex items-center justify-start gap-2 px-3 sm:px-4 pt-2 text-sm">
-            <div className={tabClass(view === 'positions')} onClick={() => setView('positions')}>
-                Open positions
-                {isBigScreen && (
-                    <div className="h-4 min-w-4 pl-1.5 pr-1.5 flex items-center justify-center text-center rounded text-xxs bg-inputcolor">
-                        {positionsCount}
-                    </div>
-                )}
-            </div>
-
-            {!isBigScreen && (
-                <div className={badgeClass(view === 'positions')}>
-                    {positionsCount}
-                </div>
-            )}
-
-            <span className="opacity-20">|</span>
-
-            <div className={tabClass(view === 'limitOrder')} onClick={() => setView('limitOrder')}>
-                Limit orders
-                {isBigScreen && (
-                    <div className="h-4 min-w-4 pl-1.5 pr-1.5 flex items-center justify-center text-center rounded text-xxs bg-inputcolor">
-                        {limitOrdersCount}
-                    </div>
-                )}
-            </div>
-
-            {!isBigScreen && (
-                <div className={badgeClass(view === 'limitOrder')}>
-                    {limitOrdersCount}
-                </div>
-            )}
-
-            <span className="opacity-20">|</span>
-
-            <span className={tabClass(view === 'history')} onClick={() => setView('history')}>
-                Trade history
-            </span>
-            {/* Action buttons */}
-            {view === 'positions' && positions?.length && onCloseAllPositions ? (
-                <Button
-                    size="xs"
-                    className={twMerge(
-                        POSITION_BLOCK_STYLES.button.filled,
-                        isBigScreen ? 'w-[13em] ml-auto' : 'w-[3em] max-w-[3em] ml-auto'
-                    )}
-                    title={isBigScreen ? "Close All Positions" : ""}
-                    icon={!isBigScreen ? crossIcon : undefined}
-                    rounded={false}
-                    onClick={onCloseAllPositions}
-                />
-            ) : null}
-            {view === 'limitOrder' && limitOrdersExist && onCancelAllLimitOrders ? (
-                <Button
-                    size="xs"
-                    className={twMerge(
-                        POSITION_BLOCK_STYLES.button.filled,
-                        isBigScreen ? 'w-[15em] ml-auto' : 'w-[3em] max-w-[3em] ml-auto'
-                    )}
-                    title={isBigScreen ? "Cancel All Limit Order" : ""}
-                    icon={!isBigScreen ? crossIcon : undefined}
-                    rounded={false}
-                    onClick={onCancelAllLimitOrders}
-                />
             ) : null}
         </div>
     );
