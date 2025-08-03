f309151687dee0025ac53fc65e05270191e1a704
Orex <adrena.orex@gmail.com>
2025-08-03 11:43:56 +0400

    Add SOLANA PERPS FEE chart

---
+++ src/components/pages/global/DefilamaProtocolFeesChart/DefilamaProtocolFeesChart.tsx
@@ -0,0 +1,119 @@
+import Tippy from '@tippyjs/react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+import defillamaImg from '@/../public/images/defillama.png';
+import StakedBarRecharts from '@/components/ReCharts/StakedBarRecharts';
+import useDefilamaFeesData from '@/hooks/useDefilamaFeesData';
+import { RechartsData } from '@/types';
+
+interface FeesChartProps {
+  isSmallScreen: boolean;
+}
+
+const protocolsToWatch = [
+  'Adrena Protocol',
+  'GMX V2 Perps',
+  'FlashTrade',
+  'Drift Trade',
+  'Jupiter Perpetual Exchange',
+];
+
+const labels = [
+  { name: 'Adrena Protocol', color: '#EE183A' },
+  { name: 'GMX V2 Perps', color: '#00b4d8' },
+  { name: 'FlashTrade', color: '#cec161' },
+  { name: 'Drift Trade', color: '#2775ca' },
+  { name: 'Jupiter Perpetual Exchange', color: '#27385b' },
+];
+
+const ADRENA_LAUNCH_DATE = Date.UTC(2024, 8, 25) / 1000; // 25 Sep 2024 (month is 0-based)
+
+export default function DefilamaProtocolFeesChart({ }: FeesChartProps) {
+  const [period, setPeriod] = useState<string | null>('3M');
+
+  const defilamaFeesData = useDefilamaFeesData();
+
+  const [mappedChartData, setMappedChartData] = useState<RechartsData[] | null>(null);
+  const [chartData, setChartData] = useState<RechartsData[] | null>(null);
+
+  useEffect(() => {
+    if (!defilamaFeesData || !Array.isArray(defilamaFeesData)) return;
+
+    const mapped: RechartsData[] = defilamaFeesData.map(([timestamp, values]) => {
+      if (timestamp < ADRENA_LAUNCH_DATE) return null;
+
+      const day = new Date(timestamp * 1000).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'numeric',
+        timeZone: 'UTC',
+      });
+
+      const defaultValues = protocolsToWatch.reduce((acc, protocol) => {
+        acc[protocol] = 0;
+        return acc;
+      }, {} as { [key: string]: number });
+
+      let totalDailyFees = 0;
+
+      const v = Object.entries(values).reduce((acc, [key, value]) => {
+        if (!protocolsToWatch.includes(key)) return acc;
+
+        acc[key] = value;
+
+        totalDailyFees += value;
+        return acc;
+      }, {
+        time: day,
+        ...defaultValues,
+      } as RechartsData);
+
+      // Ignore days with no fees
+      if (totalDailyFees === 0) return null;
+
+      // For each protocol, need to calculate the percentage of the total fees of the day
+      Object.keys(v).forEach((key) => {
+        if (key === 'time') return;
+        v[key] = ((v[key] as number) / totalDailyFees) * 100; // Convert to percentage
+      });
+
+      return v;
+    }).filter(Boolean) as RechartsData[];
+
+    setMappedChartData(mapped as RechartsData[]);
+  }, [defilamaFeesData, period]);
+
+  useEffect(() => {
+    if (!mappedChartData) return setChartData(null);
+
+    const dataPeriod = period === '7d' ? 7 : period === '1M' ? 30 : period === '3M' ? 90 : 180;
+
+    setChartData(
+      mappedChartData?.slice(-dataPeriod)
+    );
+  }, [mappedChartData, period]);
+
+  if (!chartData) return null;
+
+  return (
+    <StakedBarRecharts
+      title={<div className='flex'>
+        <Tippy content="Powered by DefiLlama API" placement="auto">
+          <div className='flex gap-2 items-center'>
+            <Image src={defillamaImg} alt="DefiLlama" className='h-6 w-6' />
+
+            <h2>SOLANA PERPS FEES</h2>
+          </div>
+        </Tippy>
+      </div>}
+      subValue={0}
+      data={chartData}
+      formatY="percentage"
+      labels={labels}
+      period={period}
+      gmt={0}
+      periods={['7d', '1M', '3M', '6M']}
+      setPeriod={setPeriod}
+    />
+  );
+}
---
+++ src/hooks/useDefilamaFeesData.tsx
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useState } from 'react';
+
+let interval: NodeJS.Timeout | null = null;
+
+const INTERVAL_IN_MS = 30_000;
+
+export type FeesData = [number, { [key: string]: number }][] | null;
+
+export default function useDefilamaFeesData(): FeesData | null {
+    const [feesData, setFeesData] = useState<FeesData | null>(null);
+
+    const loadFeesData = useCallback(async () => {
+        try {
+            const result = await fetch(
+                `https://api.llama.fi/overview/fees/solana?excludeTotalDataChartBreakdown=false`,
+            ).then((res) => res.json());
+
+            setFeesData(result.totalDataChartBreakdown);
+        } catch (e) {
+            console.log('Error loading ADX token supply', e);
+        }
+    }, []);
+
+    useEffect(() => {
+        loadFeesData();
+
+        interval = setInterval(() => {
+            loadFeesData();
+        }, INTERVAL_IN_MS);
+
+        return () => {
+            if (!interval) {
+                return;
+            }
+
+            clearInterval(interval);
+            interval = null;
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [loadFeesData]);
+
+    return feesData;
+}
