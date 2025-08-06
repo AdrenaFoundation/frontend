f9837e2f716934ea4e41aeb0c6104552b7d4ee0e
adrena-kino <adrena.kino@gmail.com>
2025-08-06 17:02:36 +0700

    fix imports

--- src/pages/buy_alp/index.tsx
+++ src/pages/buy_alp/index.tsx
@@ -1,6 +1,6 @@
 import ALPDetails from '@/components/pages/buy_alp/ALPDetails';
-import ALPSwap from '@/components/pages/buy_alp_adx/ALPSwap/ALPSwap';
 import PartnerCards from '@/components/pages/buy_alp/PartnerCards';
+import ALPSwap from '@/components/pages/buy_alp_adx/ALPSwap/ALPSwap';
 import { PageProps } from '@/types';
 
 export default function Buy({ connected }: PageProps) {
