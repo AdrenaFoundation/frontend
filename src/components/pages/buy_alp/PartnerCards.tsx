b24eddfa25e796669a0e9d0354f7716261d7d3a3
AdrenaTalin <adrena.talin@gmail.com>
2025-08-06 11:29:51 +0200

    feat: flat cards solution adopted

--- src/components/pages/buy_alp/PartnerCards.tsx
+++ src/components/pages/buy_alp/PartnerCards.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface Partner {
-  title: string;
   description: string;
-  buttonText: string;
-  buttonColor: string;
-  buttonBgColor: string;
   icon: string;
   gradientColors: string;
   bgColor: string;
@@ -18,92 +14,102 @@ const PartnerCard: React.FC<Partner> = ({
   gradientColors,
   bgColor,
   icon,
-  title,
   description,
-  buttonText,
-  buttonColor,
-  buttonBgColor,
   link,
 }) => (
-  <div className={twMerge('relative p-[1px] rounded-xl', gradientColors)}>
+  <a
+    href={link}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={twMerge('relative p-[1px] rounded-xl block', gradientColors)}
+  >
     <div
       className={twMerge(
-        'flex flex-row items-center justify-between p-4 rounded-xl h-15 relative z-10',
+        'flex flex-row items-center justify-between pt-3 pb-3 pl-5 pr-5 rounded-xl relative z-10 hover:opacity-90 transition-opacity duration-300',
         bgColor,
       )}
     >
-      <div className="flex items-center gap-3 flex-1">
-        <Image
-          src={icon}
-          alt={`${title} icon`}
-          width={title ? 20 : 80}
-          height={title ? 20 : 24}
-          className={title ? 'w-5 h-5' : 'h-4 w-auto'}
-        />
+      <div className="flex-1">
         <p className="text-sm text-white">{description}</p>
       </div>
 
-      <a
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={twMerge(
-          'w-fit px-3 py-1 rounded-full text-sm font-interMedium',
-          buttonBgColor,
-          buttonColor,
-        )}
-      >
-        {buttonText}
-      </a>
+      <div className="flex items-center gap-3">
+        <Image
+          src={icon}
+          alt="Partner logo"
+          width={20}
+          height={20}
+          className="w-[5rem] h-5 object-contain"
+        />
+        <Image
+          src="/images/Icons/arrow-sm-45.svg"
+          alt="External link"
+          width={6}
+          height={6}
+          className="w-3 h-3"
+        />
+      </div>
     </div>
-  </div>
+  </a>
 );
 
 const PARTNERS: Partner[] = [
   {
-    title: '',
     description: 'Boost your ALP with leverage loop',
-    buttonText: 'Loop Now',
-    buttonColor: 'text-orange',
-    buttonBgColor: 'bg-orange/20',
     icon: '/images/carrot.svg',
     gradientColors:
-      'bg-[linear-gradient(110deg,#17E400,#f77f00,#17E400)] animate-text-shimmer bg-[length:250%_100%]',
+      'bg-[linear-gradient(110deg,#34D399,#F98635,#34D399)] animate-text-shimmer bg-[length:250%_100%]',
     bgColor: 'bg-secondary/[0.99]',
     link: 'https://boost.deficarrot.com/tokens/GyTsdytwPU2oRK9YfpJz8Nb5auxuPegSPrTA5gWTb2o',
   },
   {
-    title: '',
-    description: 'Multiply your yield with low-risk looping',
-    buttonText: 'Loop Now',
-    buttonColor: 'text-[#908dff]',
-    buttonBgColor: 'bg-[#908dff]/20',
+    description: 'Multiply your yield with low-risk vaults',
     icon: '/images/kamino.svg',
     gradientColors:
       'bg-[linear-gradient(110deg,#2b5dff,#001763,#2b5dff)] animate-text-shimmer bg-[length:250%_100%]',
     bgColor: 'bg-secondary/[0.99]',
-    link: 'https://app.kamino.finance/multiply/CTthJu49dgPkCgu9TKQProuaQnkHsRPQV33sSJ96vxpG/D1ZdZSNfn6nfzEyK6uvFCcRi8SzihnPdYHLtnjBjwxGn/5bgPMvzZv29jkFEuMwxQRJQf64gKcPfLEEUHhyrP8tce',
+    link: 'https://app.kamino.finance/multiply/CTthJu49dgPkCgu9TKQProuaQnkHsRPQV33sSJ96vxpG/D1ZdZSNfn6uvFCcRi8SzihnPdYHLtnjBjwxGn/5bgPMvzZv29jkFEuMwxQRJQf64gKcPfLEEUHhyrP8tce',
   },
   {
-    title: '',
-    description: 'Loop your ALP with loop vaults',
-    buttonText: 'Loop Now',
-    buttonColor: 'text-blue',
-    buttonBgColor: 'bg-blue/20',
+    description: 'Loop your ALP up to 6x',
     icon: '/images/loopscale.svg',
     gradientColors:
-      'bg-[linear-gradient(110deg,#1e56ff,#7e9eff,#1e56ff)] animate-text-shimmer bg-[length:250%_100%]',
+      'bg-[linear-gradient(110deg,#c9c8ff,#0075ff,#c9c8ff)] animate-text-shimmer bg-[length:250%_100%]',
     bgColor: 'bg-secondary/[0.99]',
     link: 'https://app.loopscale.com/loops/alpLi5yWGzpTWMQ1iWHG5CrGYAdBkhyEdsuSugjDUqwj',
+  },
+  {
+    description: 'Earn Fixed Yield on ALP',
+    icon: '/images/exponent.svg',
+    gradientColors:
+      'bg-[linear-gradient(110deg,#001b03,#00ee1a,#001b03)] animate-text-shimmer bg-[length:250%_100%]',
+    bgColor: 'bg-secondary/[0.99]',
+    link: 'https://www.exponent.finance/liquidity/alp-20Oct25',
+  },
+  {
+    description: 'Incentives for ALP-SOL providers',
+    icon: '/images/meteora.svg',
+    gradientColors:
+      'bg-[linear-gradient(110deg,#ff7800,#5601d1,#ff7800)] animate-text-shimmer bg-[length:250%_100%]',
+    bgColor: 'bg-secondary/[0.99]',
+    link: 'https://www.meteora.ag/dlmm/39xxvte8BMaW7qBxeedFk9iauG42vxFsTA7yzM9X9cQN',
+  },
+  {
+    description: 'Incentives for ALP-USDC providers',
+    icon: '/images/meteora.svg',
+    gradientColors:
+      'bg-[linear-gradient(110deg,#5601d1,#ff7800,#5601d1)] animate-text-shimmer bg-[length:250%_100%]',
+    bgColor: 'bg-secondary/[0.99]',
+    link: 'https://www.meteora.ag/dlmm/4wM3eJMduZBFytW6VqV5DC2CaSovRrM2RJG8bJkroqLD',
   },
 ];
 
 export default function PartnerCards({ className }: { className?: string }) {
   return (
     <div className={twMerge('w-full', className)}>
-      <div className="grid grid-cols-1 gap-3">
-        {PARTNERS.map((partner) => (
-          <PartnerCard key={partner.title} {...partner} />
+      <div className="grid grid-cols-1 gap-2">
+        {PARTNERS.map((partner, index) => (
+          <PartnerCard key={index} {...partner} />
         ))}
       </div>
     </div>
