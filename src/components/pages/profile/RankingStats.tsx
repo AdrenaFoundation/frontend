b2628ad7ac8fffb975bdd12ec5322e4119878840
Abdirahim-A <139131552+adrena-tyro@users.noreply.github.com>
2025-08-20 09:56:14 +0400

    nit

--- src/components/pages/profile/RankingStats.tsx
+++ src/components/pages/profile/RankingStats.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
 
 import FormatNumber from '@/components/Number/FormatNumber';
