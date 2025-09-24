df0f6971ce812d1306a74ca063f3f489bc1aef46
AdrenaTalin <adrena.talin@gmail.com>
2025-09-24 16:27:10 +0200

    nit: didn't git add correctly ?

--- src/components/pages/buy_alp/ALPSwap/ALPSwapSell.tsx
+++ src/components/pages/buy_alp/ALPSwap/ALPSwapSell.tsx
@@ -53,7 +53,12 @@ export default function ALPSwapSell({
         collateralTokenCustody.pubkey.toBase58()
       ] * tokenPrice
     );
-  }, [tokenPrices, collateralToken.symbol, collateralTokenCustodyLiquidity, collateralTokenCustody.pubkey]);
+  }, [
+    tokenPrices,
+    collateralToken.symbol,
+    collateralTokenCustodyLiquidity,
+    collateralTokenCustody.pubkey,
+  ]);
 
   const [collateralPrice, setCollateralPrice] = useState<number | null>(null);
   const [collateralInputUsd, setCollateralInputUsd] = useState<number | null>(
